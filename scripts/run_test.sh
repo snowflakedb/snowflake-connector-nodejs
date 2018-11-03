6006bca5c80942433b6d9dbb4c47ca4071817e9e
Shige Takeda <smtakeda@gmail.com>
2018-11-03 03:27:58 +0000

    reordered mocha command line options

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -7,20 +7,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TEST_TIMEOUT=90000
 
-MOCHA_CMD=()
-MOCHA_OPTS=("--timeout" "$TEST_TIMEOUT")
 if [[ -z "$TRAVIS_JOB_ID" ]]; then
-    MOCHA_CMD+=(
+    MOCHA_CMD=(
         "mocha"
-    )
-    MOCHA_OPTS+=(
+        "--timeout" "$TEST_TIMEOUT"
         "--reporter" "xunit"
         "--reporter-options" "output=$DIR/../junit.xml"
     )
     rm -f $DIR/../junit*.xml
 else
-    MOCHA_CMD+=(
+    MOCHA_CMD=(
         "./node_modules/.bin/istanbul" "cover" "./node_modules/.bin/_mocha"
+        "--timeout" "$TEST_TIMEOUT"
     )
 fi
 
@@ -29,12 +27,13 @@ echo "[INFO] mocha: $(which mocha) - $(mocha --version)"
 ERR=
 if [[ -e "system_test" ]]; then
     echo "[INFO] Running System Tests"
-    ${MOCHA_CMD[@]} ${MOCHA_OPTS[@]} system_test || (echo "[ERROR] Failed." && ERR=1)
+    ${MOCHA_CMD[@]} system_test || (echo "[ERROR] Failed." && ERR=1)
     cp -f $DIR/../junit.xml $DIR/../junit-system-test.xml || true
 fi
 echo "[INFO] Running Tests"
 TESTS=$(find test -name "*.js")
-${MOCHA_CMD[@]} ${MOCHA_OPTS[@]} ${TESTS} || ERR=1
+echo "==> ${MOCHA_CMD[@]} ${TESTS}"
+${MOCHA_CMD[@]} ${TESTS} || ERR=1
 
 # exit 1 if the test failed.
 if [[ -n "$ERR" ]]; then

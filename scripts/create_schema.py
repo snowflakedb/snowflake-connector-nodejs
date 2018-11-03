2d8bc5212836a25064c89ec629517efc153d9128
Shige Takeda <smtakeda@gmail.com>
2018-11-03 01:16:21 +0000

    fixed schema name

--- scripts/create_schema.py
+++ scripts/create_schema.py
@@ -12,6 +12,6 @@ test_schema = sf_test_utils.get_test_schema()
 params = sf_test_utils.init_connection_params()
 
 con = snowflake.connector.connect(**params)
-con.cursor().execute("create or replace schema {0} clone C_API_TEST_DATA_SCHEMA".format(test_schema))
+con.cursor().execute("create or replace schema {0}".format(test_schema))
 
 sys.exit(0)

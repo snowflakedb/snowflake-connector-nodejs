d1c506fce4efed58392a1f5a99a3800fd1a41c02
Shige Takeda <smtakeda@gmail.com>
2018-11-03 01:06:37 +0000

    remove setup data

--- scripts/run_travis.sh
+++ scripts/run_travis.sh
@@ -35,10 +35,6 @@ trap finish EXIT
 
 source $DIR/env.sh
 
-travis_fold_start setup_test_data "Setting up test data"
-python $DIR/setup_test_data.py
-travis_fold_end
-
 travis_fold_start create_schema "Create test schema"
 python $DIR/create_schema.py 
 travis_fold_end

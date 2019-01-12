309d01b24e91537608146fca47fa7c068ecd6ab4
Shige Takeda <smtakeda@gmail.com>
2019-01-12 18:06:56 +0000

    Added Azure test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 set -o pipefail
 
-openssl aes-256-cbc -k "$super_secret_password" -in parameters.json.enc -out parameters.json -d
+if [[ -n "$SNOWFLAKE_AZURE" ]]; then
+    openssl aes-256-cbc -k "$super_azure_secret_password" -in parameters_az.json.enc -out parameters.json -d
+else
+    openssl aes-256-cbc -k "$super_secret_password" -in parameters.json.enc -out parameters.json -d
+fi
 pyenv local 3.6
 pip install -U pip
 pip --version

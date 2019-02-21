21e292d5cd021bc9b37feed9c1061e4e7b7f4af6
Shige Takeda <smtakeda@gmail.com>
2019-02-21 17:25:31 +0000

    Fixed testCancel

--- test/integration/testCancel.js
+++ test/integration/testCancel.js
@@ -1,54 +1,30 @@
 var async = require('async');
 var testUtil = require('./testUtil');
 
-describe('Test Cancel Query', function()
+describe('Test Cancel Query', function ()
 {
   var connection;
-  var createTestDB = 'create or replace database NodeTestDB';
-  var createTestSch = 'create or replace schema NodeTestSchema';
-  var createUser = 'create or replace user userCancel password=\'1234\'';
   var longQuery = 'select count(*) from table(generator(timeLimit => 3600))';
-  var dropTestDB = 'drop database if exists NodeTestDB';
-  var dropUser = 'drop User if exists userCancel';
 
-  before(function(done)
+  before(function (done)
   {
     connection = testUtil.createConnection();
-    async.series([
-        function(callback)
+    async.series(
+      [
+        function (callback)
         {
           testUtil.connect(connection, callback);
-        },
-        function(callback)
-        {
-          testUtil.executeCmd(connection, createTestDB, callback);
-        },
-        function(callback)
-        {
-          testUtil.executeCmd(connection, createTestSch, callback);
-        },
-        function(callback)
-        {
-          testUtil.executeCmd(connection, createUser, callback);
         }
       ],
       done
     );
   });
-  
-  after(function(done)
+
+  after(function (done)
   {
     async.series(
       [
-        function(callback)
-        {
-          testUtil.executeCmd(connection, dropTestDB, callback);
-        },
-        function(callback)
-        {
-          testUtil.executeCmd(connection, dropUser, callback);
-        },
-        function(callback)
+        function (callback)
         {
           testUtil.destroyConnection(connection, callback);
         }
@@ -56,21 +32,22 @@ describe('Test Cancel Query', function()
       done
     );
   });
-  
-  it('testCancelQuerySimple', function(done)
+
+
+  it('testCancelQuerySimple', function (done)
   {
     var statement = connection.execute({
-      sqlText: longQuery 
+      sqlText: longQuery
     });
-    
-    setTimeout(function()
+
+    setTimeout(function ()
     {
-      statement.cancel(function(err)
+      statement.cancel(function (err)
       {
         testUtil.checkError(err);
         done();
       });
-    }, 3000);
+    }, 10000);
   });
 });
 

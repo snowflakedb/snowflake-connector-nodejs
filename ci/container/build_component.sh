e1f5b0f97dd8b2b5b67bd17d3b65a83f2dc04e06
Shige Takeda <shige.takeda@snowflake.com>
2019-12-23 09:53:36 -0800

    adjusted build

--- ci/container/build_component.sh
+++ ci/container/build_component.sh
@@ -4,19 +4,16 @@
 #
 set -o pipefail
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 [[ -z "$GIT_BRANCH" ]] && echo "Set GIT_BRANCH to build" && exit 1
 [[ -z "$GIT_URL" ]] && echo "Set GIT_URL to build" && exit 1
-echo "[INFO] checking out from branch $GIT_BRANCH"
+
+echo "[INFO] Checking out branch $GIT_BRANCH from $GIT_URL."
 git clone $GIT_URL target
 cd target
 git checkout --track $GIT_BRANCH
-if [[ -n "$GIT_COMMIT" ]]; then
-    echo "[INFO] Checking out with the commit hash $GIT_COMMIT"
-    git checkout $GIT_COMMIT
-else
-    GIT_COMMIT=$(git rev-parse HEAD)
-    echo "[INFO] Resetting the commit hash to $GIT_COMMIT"
-fi
+source $THIS_DIR/get_git_commit.sh
+
 echo "[INFO] Building"
 npm pack
 npm install --package-lock-only
@@ -24,7 +21,8 @@ npm install --package-lock-only
 # We no longer use this repo and need to find out it the new version does support audit.
 rm -f ~/.npmrc
 npm audit
-PACKAGE_NAME=$(ls snowflake-sdk*)
-TS=$(TZ=UTC git show -s --date='format-local:%Y%m%dT%H%M%S' --format="%cd" $GIT_COMMIT)
-echo aws s3 cp --only-show-errors $PACKAGE_NAME s3://sfc-jenkins/repository/nodejs/$GIT_BRANCH/${TS}_${GIT_COMMIT}/
-aws s3 cp --only-show-errors $PACKAGE_NAME s3://sfc-jenkins/repository/nodejs/$GIT_BRANCH/${TS}_${GIT_COMMIT}/
+
+echo "[INFO] Uploading Artifacts"
+ARTIFACTS=($(ls snowflake-sdk*))
+export ARTIFACTS
+source $THIS_DIR/upload_artifact.sh
---
+++ ci/container/upload_artifact.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+#
+# Upload Artifact
+#
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+[[ -z "$DRIVER_NAME" ]] && echo "Set DRIVER_NAME to upload the artifact" && exit 1
+[[ -z "$GIT_BRANCH" ]] && echo "Set GIT_BRANCH to upload the artifact" && exit 1
+[[ -z "$GIT_COMMIT" ]] && echo "Set GIT_COMMIT to upload the artifact" && exit 1
+[[ -z "$ARTIFACTS" ]] && echo "Set ARTIFACTS to upload the artifact" && exit 1
+
+if ! git status; then
+    echo "[ERROR] 'j
+fi
+TS=$(TZ=UTC git show -s --date='format-local:%Y%m%dT%H%M%S' --format="%cd" $GIT_COMMIT)
+for $f in ${ARTIFACTS[@]}; do
+    echo "[INFO] aws s3 cp --only-show-errors $f s3://sfc-jenkins/repository/$DRIVER_NAME/$GIT_BRANCH/${TS}_${GIT_COMMIT}/"
+    aws s3 cp --only-show-errors $f s3://sfc-jenkins/repository/$DRIVER_NAME/$GIT_BRANCH/${TS}_${GIT_COMMIT}/
+done

3715823327e7c0b5ce7da73860e20fee5053eede
Shige Takeda <shige.takeda@snowflake.com>
2019-12-23 09:53:36 -0800

    adjusted build

--- ci/container/upload_artifact.sh
+++ ci/container/upload_artifact.sh
@@ -9,11 +9,18 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [[ -z "$ARTIFACTS" ]] && echo "Set ARTIFACTS to upload the artifact" && exit 1
 
 if ! git status; then
+<<<<<<< HEAD
     echo "[ERROR] Must be in the GIT repo directory."
 fi
 TS=$(TZ=UTC git show -s --date='format-local:%Y%m%dT%H%M%S' --format="%cd" $GIT_COMMIT)
 for f in "${ARTIFACTS[@]}"; do
     echo $f
+=======
+    echo "[ERROR] 'j
+fi
+TS=$(TZ=UTC git show -s --date='format-local:%Y%m%dT%H%M%S' --format="%cd" $GIT_COMMIT)
+for $f in ${ARTIFACTS[@]}; do
+>>>>>>> adjusted build
     echo "[INFO] aws s3 cp --only-show-errors $f s3://sfc-jenkins/repository/$DRIVER_NAME/$GIT_BRANCH/${TS}_${GIT_COMMIT}/"
     aws s3 cp --only-show-errors $f s3://sfc-jenkins/repository/$DRIVER_NAME/$GIT_BRANCH/${TS}_${GIT_COMMIT}/
 done

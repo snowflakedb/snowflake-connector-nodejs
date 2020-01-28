e8f5465aa9f8cf2f8200243b3c3bc1422b66090e
Shige Takeda <shige.takeda@snowflake.com>
2020-01-28 23:51:15 +0000

    test

--- scripts/install_github_workflow.sh
+++ scripts/install_github_workflow.sh
@@ -6,7 +6,7 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Decrypt the parameters.json"
 gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output $THIS_DIR/../parameters.json $THIS_DIR/../.github/workflows/parameters_aws.json.gpg
 
-source $THIS_DIR/../env.sh
+source $THIS_DIR/env.sh
 
 pyenv local 3.6
 pip install -U pip

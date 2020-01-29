5897f9f99b1a764a66fa6b290181edfbf3678e43
Shige Takeda <shige.takeda@snowflake.com>
2020-01-29 05:29:19 +0000

    add GITHUB_SHA

--- ci/test.sh
+++ ci/test.sh
@@ -92,6 +92,7 @@ for name in "${!TARGET_TEST_IMAGES[@]}"; do
         -e AWS_ACCESS_KEY_ID \
         -e AWS_SECRET_ACCESS_KEY \
         -e GITHUB_ACTIONS \
+        -e GITHUB_SHA \
         "${TARGET_TEST_IMAGES[$name]}" \
         "/mnt/host/container/test_component.sh"
 done

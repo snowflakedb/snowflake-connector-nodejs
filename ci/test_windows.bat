2d3690880657d36baae1305fd65830234c2d114d
Shige Takeda <shige.takeda@snowflake.com>
2020-01-30 01:11:51 +0000

    win test

--- ci/test_windows.bat
+++ ci/test_windows.bat
@@ -1,2 +1,11 @@
 cd %GITHUB_WORKSPACE%
 gpg --quiet --batch --yes --decrypt --passphrase=%PARAMETERS_SECRET% --output parameters.json .github/workflows/parameters_aws.json.gpg
+dir
+jq
+choco install jq
+jq
+python3 --version
+dir
+dir artifacts
+copy ci\container\package.json .
+npm install

01d50c741b8786f8c6f84fecc8311a6355c90bf9
Shige Takeda <shige.takeda@snowflake.com>
2020-01-30 01:05:02 +0000

    fix gpg

--- ci/test_windows.bat
+++ ci/test_windows.bat
@@ -1,3 +1,2 @@
 cd %GITHUB_WORKSPACE%
-gpg --quiet --batch --yes -decrypt --passphrase=%PARAMETERS_SECRET% --output parameters.json .github/workflows/parameters_aws.json.gpg
-dir
+gpg --quiet --batch --yes --decrypt --passphrase=%PARAMETERS_SECRET% --output parameters.json .github/workflows/parameters_aws.json.gpg

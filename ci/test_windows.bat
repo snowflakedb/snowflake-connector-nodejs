01f0074058be97bfe847beec67916875c8420951
Shige Takeda <shige.takeda@snowflake.com>
2020-01-30 01:35:59 +0000

    add tests

--- ci/test_windows.bat
+++ ci/test_windows.bat
@@ -1,11 +1,15 @@
 cd %GITHUB_WORKSPACE%
 gpg --quiet --batch --yes --decrypt --passphrase=%PARAMETERS_SECRET% --output parameters.json .github/workflows/parameters_aws.json.gpg
-dir
-jq
-choco install jq
-jq
-python3 --version
-dir
-dir artifacts
+copy artifacts\* .
 copy ci\container\package.json .
+
+echo @echo off>parameters.bat
+jq -r ".testconnection | to_entries | map(\"set \(.key)=\(.value)\") | .[]" parameters.json >> parameter.bat
+call parameters.bat
+if %ERRORLEVEL% NEQ 0 (
+    echo === failed to set the test parameters
+    exit /b 1
+)
 npm install
+for %%f in (snowflake-sdk*.tgz) do npm install %%f
+mocha --timeout 90000 --recursive --full-trace --color test/**/*.js

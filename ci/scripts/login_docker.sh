066e3a7fa9be8bba3d458c48fcbb5d4722c24e91
Shige Takeda <shige.takeda@snowflake.com>
2020-03-30 16:47:39 -0700

    Prep 1.5.1 (#93)
    * SNOW-148136 update minimist (#91)
    
    * SNOW-148046: Fix Date Bind Parameter
    
    * SNOW-148301: Bumped up the NodeJS Driver PATCH version from 1.5.0 to 1.5.1

--- ci/scripts/login_docker.sh
+++ ci/scripts/login_docker.sh
@@ -4,7 +4,7 @@
 #
 echo "[INFO] Login the Docker Hub"
 if [[ -z "$DOCKER_HUB_USER" ]] || [[ -z "$DOCKER_HUB_TOKEN" ]]; then
-    echo "[ERROR] Set DOCKER_HUB_USER and DOCKER_HUB_Token to push the images to the Docker Hub"
+    echo "[ERROR] Set DOCKER_HUB_USER and DOCKER_HUB_TOKEN to push the images to the Docker Hub"
     exit 1
 fi
 docker login --username "$DOCKER_HUB_USER" --password "$DOCKER_HUB_TOKEN"

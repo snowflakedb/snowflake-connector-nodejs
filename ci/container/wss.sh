7357899f58431f26cf4bb5eaa030fffa141b3258
Shige Takeda <shige.takeda@snowflake.com>
2020-06-01 19:38:47 +0000

    update the name

--- ci/container/wss.sh
+++ ci/container/wss.sh
@@ -8,8 +8,8 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 [[ -z "$WHITESOURCE_API_KEY" ]] && echo "[WARNING] No WHITESOURCE_API_KEY is set. No WhiteSource scan will occur." && exit 0
 
-export PRODUCT_NAME=NodeJSDriver
-export PROJECT_NAME=NodeJSDriver
+export PRODUCT_NAME=snowflake-connector-nodejs
+export PROJECT_NAME=snowflake-connector-nodejs
 
 DATE=$(date +'%m-%d-%Y')
 

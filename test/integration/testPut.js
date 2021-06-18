989d9d9fc0a8bbbf66abcaa798294076e4d2db4c
SimbaGithub <SimbaGithub@magnitude.com>
2021-06-18 16:43:44 -0700

    Fix passing an incorrect encoding and remove using tmp

--- test/integration/testPut.js
+++ test/integration/testPut.js
@@ -8,12 +8,11 @@ const connOption = require('./connectionOptions');
 const fileCompressionType = require('./../../lib/file_transfer_agent/file_compression_type');
 const fs = require('fs');
 const testUtil = require('./testUtil');
-const tmp = require('tmp');
 
 const DATABASE_NAME = connOption.valid.database;
 const SCHEMA_NAME = connOption.valid.schema;
 const TEMP_TABLE_NAME = 'TEMP_TABLE';
-
+const TEMP_FILE_NAME = 'TEMP_FILE_NAME';
 const UPLOADED = "UPLOADED";
 
 const COL1 = 'C1';
@@ -27,7 +26,6 @@ const ROW_DATA =
   COL1_DATA + "," + COL2_DATA + "," + COL3_DATA + "\n" +
   COL1_DATA + "," + COL2_DATA + "," + COL3_DATA + "\n" +
   COL1_DATA + "," + COL2_DATA + "," + COL3_DATA + "\n";
-
 
 describe('PUT test', function ()
 {
@@ -53,8 +51,7 @@ describe('PUT test', function ()
 
   afterEach(function ()
   {
-    fs.closeSync(tmpFile.fd);
-    fs.rmSync(tmpFile.name);
+    fs.rmSync(tmpFile);
   });
 
   var testCases =
@@ -85,117 +82,123 @@ describe('PUT test', function ()
       }
     ];
 
+  var createItCallback = function (testCase)
+  {
+    return function (done)
+    {
+      {
+        // Write row data to temp file
+        tmpFile = TEMP_FILE_NAME + testCase.encoding['file_extension'];
+        fs.writeFileSync(tmpFile, ROW_DATA);
+
+        async.series(
+          [
+            function (callback)
+            {
+              // Create temp table
+              testUtil.executeCmd(connection, createTable, callback);
+            },
+            function (callback)
+            {
+              // Upload file
+              var statement = connection.execute({
+                sqlText: `PUT file://${tmpFile} @${DATABASE_NAME}.${SCHEMA_NAME}.%${TEMP_TABLE_NAME}`,
+                complete: function (err, stmt, rows)
+                {
+                  var stream = statement.streamRows();
+                  stream.on('error', function (err)
+                  {
+                    done(err);
+                  });
+                  stream.on('data', function (row)
+                  {
+                    // Check the file is correctly uploaded
+                    assert.strictEqual(row['status'], UPLOADED);
+                    // Check the target encoding is correct
+                    assert.strictEqual(row['targetCompression'], testCase.encoding['name']);
+                  });
+                  stream.on('end', function (row)
+                  {
+                    callback();
+                  });
+                }
+              });
+            },
+            function (callback)
+            {
+              // Copy into temp table
+              testUtil.executeCmd(connection, copyInto, callback);
+            },
+            function (callback)
+            {
+              // Check the contents are correct
+              var statement = connection.execute({
+                sqlText: `SELECT * FROM ${TEMP_TABLE_NAME}`,
+                complete: function (err, stmt, rows)
+                {
+                  var stream = statement.streamRows();
+                  stream.on('error', function (err)
+                  {
+                    done(err);
+                  });
+                  stream.on('data', function (row)
+                  {
+                    // Check the row data is correct
+                    assert.strictEqual(row[COL1], COL1_DATA);
+                    assert.strictEqual(row[COL2], COL2_DATA);
+                    assert.strictEqual(row[COL3], COL3_DATA);
+                  });
+                  stream.on('end', function (row)
+                  {
+                    callback()
+                  });
+                }
+              });
+            },
+            function (callback)
+            {
+              // Check the row count is correct
+              var statement = connection.execute({
+                sqlText: `SELECT COUNT(*) FROM ${TEMP_TABLE_NAME}`,
+                complete: function (err, stmt, rows)
+                {
+                  var stream = statement.streamRows();
+                  stream.on('error', function (err)
+                  {
+                    done(err);
+                  });
+                  stream.on('data', function (row)
+                  {
+                    // Check the row count is correct
+                    assert.strictEqual(row['COUNT(*)'], 4);
+                  });
+                  stream.on('end', function (row)
+                  {
+                    callback();
+                  });
+                }
+              });
+            },
+            function (callback)
+            {
+              // Remove files from staging
+              testUtil.executeCmd(connection, removeFile, callback);
+            },
+            function (callback)
+            {
+              // Drop temp table
+              testUtil.executeCmd(connection, dropTable, callback);
+            }
+          ],
+          done
+        );
+      };
+    };
+  };
+
   for (var index = 0; index < testCases.length; index++)
   {
     var testCase = testCases[index];
-    it(testCase.name, function (done)
-    {
-      // Create a temp file with specified file extension
-      tmpFile = tmp.fileSync({ postfix: testCase.encoding['file_extension'] });
-      // Write row data to temp file
-      fs.writeFileSync(tmpFile.name, ROW_DATA);
-
-      async.series(
-        [
-          function (callback)
-          {
-            // Create temp table
-            testUtil.executeCmd(connection, createTable, callback);
-          },
-          function (callback)
-          {
-            // Upload file
-            var statement = connection.execute({
-              sqlText: `PUT file://${tmpFile.name} @${DATABASE_NAME}.${SCHEMA_NAME}.%${TEMP_TABLE_NAME}`,
-              complete: function (err, stmt, rows)
-              {
-                var stream = statement.streamRows();
-                stream.on('error', function (err)
-                {
-                  done(err);
-                });
-                stream.on('data', function (row)
-                {
-                  // Check the file is correctly uploaded
-                  assert.strictEqual(row['status'], UPLOADED);
-                  // Check the target encoding is correct
-                  assert.strictEqual(row['targetCompression'], testCase.encoding['name']);
-                });
-                stream.on('end', function (row)
-                {
-                  callback();
-                });
-              }
-            });
-          },
-          function (callback)
-          {
-            // Copy into temp table
-            testUtil.executeCmd(connection, copyInto, callback);
-          },
-          function (callback)
-          {
-            // Check the contents are correct
-            var statement = connection.execute({
-              sqlText: `SELECT * FROM ${TEMP_TABLE_NAME}`,
-              complete: function (err, stmt, rows)
-              {
-                var stream = statement.streamRows();
-                stream.on('error', function (err)
-                {
-                  done(err);
-                });
-                stream.on('data', function (row)
-                {
-                  // Check the row data is correct
-                  assert.strictEqual(row[COL1], COL1_DATA);
-                  assert.strictEqual(row[COL2], COL2_DATA);
-                  assert.strictEqual(row[COL3], COL3_DATA);
-                });
-                stream.on('end', function (row)
-                {
-                  callback()
-                });
-              }
-            });
-          },
-          function (callback)
-          {
-            // Check the row count is correct
-            var statement = connection.execute({
-              sqlText: `SELECT COUNT(*) FROM ${TEMP_TABLE_NAME}`,
-              complete: function (err, stmt, rows)
-              {
-                var stream = statement.streamRows();
-                stream.on('error', function (err)
-                {
-                  done(err);
-                });
-                stream.on('data', function (row)
-                {
-                  // Check the row count is correct
-                  assert.strictEqual(row['COUNT(*)'], 4);
-                });
-                stream.on('end', function (row)
-                {
-                  callback();
-                });
-              }
-            });
-          },
-          function (callback)
-          {
-            // Remove files from staging
-            testUtil.executeCmd(connection, removeFile, callback);
-          },
-          function (callback)
-          {
-            // Drop temp table
-            testUtil.executeCmd(connection, dropTable, callback);
-          }
-        ],
-        done
-      );
-    });
+    it(testCase.name, createItCallback(testCase));
   }
 });

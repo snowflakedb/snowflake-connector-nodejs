862b02363e05d3163ebbbb5f35e86f57cb0be0bf
SimbaGithub <SimbaGithub@magnitude.com>
2022-08-24 10:00:15 -0700

    debug

--- ci/test.sh
+++ ci/test.sh
@@ -25,17 +25,13 @@ source $THIS_DIR/scripts/set_git_info.sh
 export NVM_DIR=/usr/local/nvm
 export NODE_VERSION=14.20.0
 
-sudo mkdir -p $NVM_DIR
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | sudo bash
 
 # node
-source $NVM_DIR/nvm.sh \
-    && sudo nvm install $NODE_VERSION \
-    && sudo nvm alias default $NODE_VERSION \
-    && sudo nvm use $NODE_VERSION
+sudo nvm ls
+sudo nvm install $NODE_VERSION
+sudo nvm use $NODE_VERSION
 
-export NODE_PATH=$NVM_DIR/v$NODE_VERSION/lib/node_modules
-export PATH=$NVM_DIR/versions/node/v$NODE_VERSION/bin:$PATH
 sudo npm install npm@latest -g
 
 echo "[INFO] Creating a subnet for tests"

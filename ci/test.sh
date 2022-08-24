af7d390700a61ac5be79c6d6694dc65a1c75c022
SimbaGithub <SimbaGithub@magnitude.com>
2022-08-24 10:24:04 -0700

    debug

--- ci/test.sh
+++ ci/test.sh
@@ -24,8 +24,6 @@ source $THIS_DIR/scripts/set_git_info.sh
 # nvm environment variables
 
 export NVM_DIR=/home/runner/.nvm
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 source $NVM_DIR/nvm.sh
 

0c820e5910d0f14cfe130d8706a88887a229b8ba
SimbaGithub <SimbaGithub@magnitude.com>
2022-08-25 08:49:37 -0700

    debug

--- ci/image/build.sh
+++ ci/image/build.sh
@@ -13,3 +13,11 @@ for name in "${!BUILD_IMAGE_NAMES[@]}"; do
         --label $DRIVER_NAME \
         --tag ${BUILD_IMAGE_NAMES[$name]} .
 done
+
+for name in "${!TEST_IMAGE_NAMES[@]}"; do
+    docker build \
+        --file $THIS_DIR/Dockerfile.$name-test \
+        --label snowflake \
+        --label $DRIVER_NAME \
+        --tag ${TEST_IMAGE_NAMES[$name]} .
+done

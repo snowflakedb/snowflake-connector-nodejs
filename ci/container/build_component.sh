4a4cb237cd772c7a6b3261744861d0f6d6b6a557
SimbaGithub <SimbaGithub@magnitude.com>
2022-08-24 17:00:57 -0700

    debug

--- ci/container/build_component.sh
+++ ci/container/build_component.sh
@@ -3,7 +3,12 @@
 # Build NodeJS Driver
 #
 set -o pipefail
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+[[ -z "$GIT_BRANCH" ]] && echo "Set GIT_BRANCH to build" && exit 1
+[[ -z "$GIT_URL" ]] && echo "Set GIT_URL to build" && exit 1
+
+cd /mnt/host
 echo "[INFO] Building"
 rm -f snowflake-sdk*.tgz
 npm pack
@@ -16,3 +21,4 @@ echo "[INFO] Uploading Artifacts"
 ARTIFACTS=($(ls snowflake-sdk*))
 export ARTIFACTS
 export DRIVER_NAME=nodejs
+source $THIS_DIR/upload_artifact.sh

d5f1dc1062b3758beb084c818ad8d8c3d723d9d1
Dicky Lau <dlau@magnitude.com>
2023-01-25 16:24:22 -0800

    add server version and driver version on the log

--- test/integration/testMultiStatement.js
+++ test/integration/testMultiStatement.js
@@ -4,6 +4,7 @@
 var async = require('async');
 var assert = require('assert');
 var testUtil = require('./testUtil');
+var Util = require('./../../lib/util');
 
 
 describe('Test multi statement', function ()
@@ -36,39 +37,56 @@ describe('Test multi statement', function ()
     testUtil.destroyConnection(connection, done);
   });
 
-  it('testMultiStatement', function(done)
-  {
-	var bindArr = [1,2,4];
-	var count=0;
+    it('testMultiStatement', function (done) {
+        async.series(
+            [
+                function (callback) {
+                    connection = testUtil.createConnection();
+                    testUtil.connect(connection, function () {
+                        connection.execute({
+                            sqlText: 'select current_version()',
+                            complete: function (err, stmt, rows) {
+                                console.log('=== driver version = ' + Util.driverVersion);
+                                console.log('=== server version =');
+                                console.log(rows);
+                                callback();
+                            }
+                        });
+                    });
+                },
+                function (callback) {
+                    var bindArr = [1, 2, 4];
+                    var count = 0;
 
-	connection = testUtil.createConnection();
-    testUtil.connect(connection, function ()
-    {
-      connection.execute({
-        sqlText: selectTable,
-		binds: bindArr,
-        complete: function (err, stmt)
-        {
-          	testUtil.checkError(err);
-		  	var stream = stmt.streamRows();
-			stream.on('error', function (err) {
-				testUtil.checkError(err);
-			});
-			stream.on('data', function (row) {
-				count += Object.values(row).length;
-				if (stmt.hasNext()) {
-					console.log('==== hasNext');
-					stmt.NextResult();
-				}
-				else
-				{
-					console.log('==== close connection');
-					assert.strictEqual(6, count);
-					CloseConnection();
-				}
-			});
-        }
-      });
-    });
+                    connection = testUtil.createConnection();
+                    testUtil.connect(connection, function () {
+                        connection.execute({
+                            sqlText: selectTable,
+                            binds: bindArr,
+                            complete: function (err, stmt) {
+                                testUtil.checkError(err);
+                                var stream = stmt.streamRows();
+                                stream.on('error', function (err) {
+                                    testUtil.checkError(err);
+                                });
+                                stream.on('data', function (row) {
+                                    count += Object.values(row).length;
+                                    if (stmt.hasNext()) {
+                                        console.log('==== hasNext');
+                                        stmt.NextResult();
+                                    }
+                                    else {
+                                        console.log('==== close connection');
+                                        assert.strictEqual(6, count);
+                                        CloseConnection();
+                                    }
+                                });
+                            }
+                        });
+                    });
+                }
+            ],
+            done
+        );
   });
 });

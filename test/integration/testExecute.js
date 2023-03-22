92eba85cb6aea65dcb8ca8270c8ab675ee0dd004
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2023-03-22 16:16:31 -0700

    Add xml parser for raw xml values

--- test/integration/testExecute.js
+++ test/integration/testExecute.js
@@ -1,8 +1,13 @@
 /*
  * Copyright (c) 2015-2019 Snowflake Computing Inc. All rights reserved.
  */
+
+var assert = require('assert');
 var async = require('async');
+var connOption = require('./connectionOptions').valid;
 var testUtil = require('./testUtil');
+var fs = require('fs');
+var tmp = require('tmp');
 
 describe('Execute test', function ()
 {
@@ -158,3 +163,165 @@ describe('Execute test', function ()
   });
 });
 
+describe('Execute test - variant', function ()
+{
+  this.timeout(10000);
+
+  var connection;
+
+  const DATABASE_NAME = connOption.database;
+  const SCHEMA_NAME = connOption.schema;
+
+  const TEST_VARIANT_TABLE = "TEST_VARIANT_TABLE";
+  const TEST_VARIANT_STAGE = "TEST_VARIANT_STAGE";
+  const TEST_VARIANT_FORMAT = "TEST_VARIANT_FORMAT";
+  const TEST_COL = "COL";
+  const TEST_HEADER = "ROOT";
+  const TEST_VAL = 123;
+
+  before(function (done)
+  {
+    connection = testUtil.createConnection();
+    async.series([
+        function (callback)
+        {
+          testUtil.connect(connection, callback);
+        }],
+      done
+    );
+  });
+
+  after(function (done)
+  {
+    async.series([
+        function (callback)
+        {
+          testUtil.destroyConnection(connection, callback);
+        }],
+      done
+    );
+  });
+
+  var testCases =
+    [
+      {
+        name: 'raw xml',
+        type: 'XML',
+        fileExtension: '.xml',
+      },
+      {
+        name: 'raw json',
+        type: 'JSON',
+        fileExtension: '.json',
+      }
+    ];
+
+  var createItCallback = function (testCase)
+  {
+    return function (done)
+    {
+      {
+        var createTableVariant = `create or replace table ${TEST_VARIANT_TABLE}(${TEST_COL} variant)`;
+        var createFileFormatVariant = `CREATE OR REPLACE FILE FORMAT ${TEST_VARIANT_FORMAT} TYPE = ${testCase.type}`;
+        var createStageVariant = `CREATE OR REPLACE STAGE ${TEST_VARIANT_STAGE} FILE_FORMAT = ${TEST_VARIANT_FORMAT}`;
+
+        var sampleData;
+        if (testCase.type == 'XML')
+        {
+          sampleData = `<${TEST_HEADER}>${TEST_VAL}</${TEST_HEADER}>`;
+        }
+        else if (testCase.type == 'JSON')
+        {
+          sampleData = `{${TEST_HEADER}: ${TEST_VAL}}`;
+        }
+
+        var sampleTempFile = tmp.fileSync({ postfix: testCase.fileExtension });
+        fs.writeFileSync(sampleTempFile.name, sampleData);
+
+        var putVariant = `PUT file://${sampleTempFile.name} @${DATABASE_NAME}.${SCHEMA_NAME}.${TEST_VARIANT_STAGE}`;
+        var copyIntoVariant = `COPY INTO ${TEST_VARIANT_TABLE} FROM @${DATABASE_NAME}.${SCHEMA_NAME}.${TEST_VARIANT_STAGE}`;
+
+        var selectVariant = `select ${TEST_COL} from ${TEST_VARIANT_TABLE}`;
+        var dropStageVariant = `drop table if exists ${TEST_VARIANT_STAGE}`;
+        var dropTableVariant = `drop table if exists ${TEST_VARIANT_TABLE}`;
+
+        async.series(
+          [
+            function (callback)
+            {
+              // Create variant table
+              testUtil.executeCmd(connection, createTableVariant, callback);
+            },
+            function (callback)
+            {
+              // Create variant file format
+              testUtil.executeCmd(connection, createFileFormatVariant, callback);
+            },
+            function (callback)
+            {
+              // Create stage with variant file format
+              testUtil.executeCmd(connection, createStageVariant, callback);
+            },
+            function (callback)
+            {
+              // Upload sample file
+              testUtil.executeCmd(connection, putVariant, callback);
+            },
+            function (callback)
+            {
+              // Load sample file
+              testUtil.executeCmd(connection, copyIntoVariant, callback);
+            },
+            function (callback)
+            {
+              // Select values from table
+              connection.execute({
+                sqlText: selectVariant,
+                complete: function (err, stmt, rows)
+                {
+                  var stream = stmt.streamRows();
+                  stream.on('error', function (err)
+                  {
+                    done(err);
+                  });
+                  stream.on('data', function (row)
+                  {
+                    // Check the column, header, and value is correct
+                    assert.strictEqual(row[TEST_COL][TEST_HEADER], TEST_VAL);
+                  });
+                  stream.on('end', function ()
+                  {
+                    callback();
+                  });
+                }
+              });
+            },
+            function (callback)
+            {
+              // Drop stage
+              testUtil.executeCmd(connection, dropStageVariant, callback);
+            },
+            function (callback)
+            {
+              // Drop table
+              testUtil.executeCmd(connection, dropTableVariant, callback);
+            },
+            function (callback)
+            {
+              // Delete temp files
+              fs.closeSync(sampleTempFile.fd);
+              fs.unlinkSync(sampleTempFile.name);
+              callback();
+            }],
+          done
+        );
+      };
+    };
+  };
+
+  for (var index = 0; index < testCases.length; index++)
+  {
+    var testCase = testCases[index];
+    it(testCase.name, createItCallback(testCase));
+  }
+});

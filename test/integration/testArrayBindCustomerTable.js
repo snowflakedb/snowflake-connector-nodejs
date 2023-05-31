e1fb1b7f6deaaad0b0d3cdc0eb0c15c750df0a90
Dominik Przybysz <dominik.przybysz@snowflake.com>
2023-05-31 10:49:54 +0200

    SNOW-830232: Lower row number to insert in testArrayBindCustomerTable test

--- test/integration/testArrayBindCustomerTable.js
+++ test/integration/testArrayBindCustomerTable.js
@@ -1,66 +1,57 @@
 /*
  * Copyright (c) 2015-2019 Snowflake Computing Inc. All rights reserved.
  */
-var async = require('async');
 var assert = require('assert');
 var testUtil = require('./testUtil');
-
-const sourceRowCount = 30000;
 
 describe('Test Stage Binding with Customer Table', function ()
 {
   this.timeout(300000);
-  var connection;
-  var createTable = 'create or replace TABLE EVENTS_TEMP (ORGANIZATION_ID VARCHAR(16777216),' +
-	'APP_ID VARCHAR(16777216), OCCURREDAT VARCHAR(16777216), SHOP_ID VARCHAR(16777216),' +
-	'TYPE VARCHAR(16777216),ID VARCHAR(16777216),CHARGE_AMOUNT_AMOUNT FLOAT,' +
-	'CHARGE_AMOUNT_CURRENCYCODE VARCHAR(16777216),CHARGE_ID VARCHAR(16777216),' +
-	'CHARGE_NAME VARCHAR(16777216),CHARGE_TEST VARCHAR(16777216),CHARGE_BILLINGON VARCHAR(16777216),' +
-	'REASON VARCHAR(16777216),DESCRIPTION VARCHAR(16777216),APPCREDIT_AMOUNT_AMOUNT FLOAT,' +
-	'APPCREDIT_AMOUNT_CURRENCYCODE VARCHAR(16777216),APPCREDIT_ID VARCHAR(16777216),'+
-	'APPCREDIT_NAME VARCHAR(16777216),APPCREDIT_TEST VARCHAR(16777216),APP_NAME VARCHAR(16777216),'+
-	'SHOP_MYSHOPIFYDOMAIN VARCHAR(16777216),SHOP_NAME VARCHAR(16777216),APP_APIKEY VARCHAR(16777216))';
-  var insertWithQmark = 'insert into EVENTS_TEMP values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+  let connection;
+  const tableName = 'EVENTS_TEMP';
+  const createTable = `create or replace TABLE ${tableName} (ORGANIZATION_ID VARCHAR(16777216),` +
+    'APP_ID VARCHAR(16777216), OCCURREDAT VARCHAR(16777216), SHOP_ID VARCHAR(16777216),' +
+    'TYPE VARCHAR(16777216),ID VARCHAR(16777216),CHARGE_AMOUNT_AMOUNT FLOAT,' +
+    'CHARGE_AMOUNT_CURRENCYCODE VARCHAR(16777216),CHARGE_ID VARCHAR(16777216),' +
+    'CHARGE_NAME VARCHAR(16777216),CHARGE_TEST VARCHAR(16777216),CHARGE_BILLINGON VARCHAR(16777216),' +
+    'REASON VARCHAR(16777216),DESCRIPTION VARCHAR(16777216),APPCREDIT_AMOUNT_AMOUNT FLOAT,' +
+    'APPCREDIT_AMOUNT_CURRENCYCODE VARCHAR(16777216),APPCREDIT_ID VARCHAR(16777216),' +
+    'APPCREDIT_NAME VARCHAR(16777216),APPCREDIT_TEST VARCHAR(16777216),APP_NAME VARCHAR(16777216),' +
+    'SHOP_MYSHOPIFYDOMAIN VARCHAR(16777216),SHOP_NAME VARCHAR(16777216),APP_APIKEY VARCHAR(16777216))';
+  const insertWithQmark = `insert into ${tableName} values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-  before(function (done)
+  before(async () =>
   {
     connection = testUtil.createConnection();
-    testUtil.connect(connection, function ()
-    {
-      connection.execute({
-        sqlText: createTable,
-        complete: function (err)
-        {
-          testUtil.checkError(err);
-          done();
-        }
-      });
-    });
+    await testUtil.connectAsync(connection);
+    await testUtil.executeCmdAsync(connection, createTable);
   });
 
-  after(function (done)
+  after(async () =>
   {
-    testUtil.destroyConnection(connection, done);
+    await testUtil.executeCmdAsync(connection, `DROP TABLE IF EXISTS ${tableName};`);
+    await testUtil.destroyConnectionAsync(connection);
   });
 
   it('testArrayBindCustomerTable', function (done)
   {
-    var arrBind = [];
-    var count = 100000;
-    for(var i = 0; i<count; i++)
+    const arrBind = [];
+    const rowsToInsert = 10000; // 100000 rows causes "Statement exhausted compilation resources and was canceled. Please contact support." in regression environment
+    for (let i = 0; i < rowsToInsert; i++)
     {
-        arrBind.push(['string'+i, 'appid', "occuredat", "shopid", "type", "id", 10.9, "charge amount currency code",
-        'chargeid','chargename','chargetest','chargebillingon', 'reason', 'description', 99.99, 'appcredit amount currency code',
-        'appcreditid','appcreditname','appcredittest','appname','shopmyshopifyoumin','shopname','appapikey']);
+      arrBind.push(['string' + i, 'appid', 'occuredat', 'shopid', 'type', 'id', 10.9, 'charge amount currency code',
+        'chargeid', 'chargename', 'chargetest', 'chargebillingon', 'reason', 'description', 99.99, 'appcredit amount currency code',
+        'appcreditid', 'appcreditname', 'appcredittest', 'appname', 'shopmyshopifyoumin', 'shopname', 'appapikey']);
     }
-    var insertStatement = connection.execute({
-        sqlText: insertWithQmark,
-        binds: arrBind,
-        complete: function (err, stmt) {
-            testUtil.checkError(err);
-            assert.strictEqual(stmt.getNumUpdatedRows(), count);
-            done();
-        }
+    connection.execute({
+      sqlText: insertWithQmark,
+      binds: arrBind,
+      complete: function (err, stmt)
+      {
+        testUtil.checkError(err);
+        assert.strictEqual(stmt.getNumUpdatedRows(), rowsToInsert);
+        done();
+      }
     });
   });
 });

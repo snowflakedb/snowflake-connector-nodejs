9909b2b5a97332ba254260f50b064d51f9fae436
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2023-06-15 11:44:47 -0700

    issue 366 - Temp revert wrapping test in describe

--- test/integration/testLargeResultSet.js
+++ test/integration/testLargeResultSet.js
@@ -114,135 +114,129 @@ describe('Large result Set Tests', function ()
     });
   });
 
-  describe('Large Result Set Tests For Variant Column Type', function ()
+  it('testSelectOnVariantColumnForLargeResultSets', function (done)
   {
     const createTableWithVariant = 'create or replace table testVariantTable(colA variant)';
+    const insertVariant = 'insert into testVariantTable select value from table(flatten(parse_json(?)))';
+    const selectVariant = 'select * from testVariantTable';
     const dropTableWithVariant = 'drop table if exists testVariantTable';
 
-    before(function (done)
-    {
-      testUtil.executeCmd(connection, createTableWithVariant, done);
-    });
-
-    after(function (done)
-    {
-      testUtil.executeCmd(connection, dropTableWithVariant, done);
-    });
-
-    it('testSelectOnVariantColumnForLargeResultSets', function (done)
-    {
-      const insertVariant = 'insert into testVariantTable select value from table(flatten(parse_json(?)))';
-      const selectVariant = 'select * from testVariantTable';
-
-      const arrJSON = [];
-      const sampleJSON = {
-        "root":
-        {
-          "key":
-            [
+    const arrJSON = [];
+    const sampleJSON = {
+      "root":
+      {
+        "key":
+          [
+            {
+              "key1": "value1",
+              "key2": "value2",
+              "key3": "value3",
+              "key4": "value4",
+              "key5":
               {
-                "key1": "value1",
-                "key2": "value2",
-                "key3": "value3",
-                "key4": "value4",
-                "key5":
-                {
-                  "key":
-                    [
-                      { "key1": "value1", "key2": "value2" },
-                      { "key1": "value1", "key2": "value2" },
-                      { "key1": "value1", "key2": "value2" },
-                      { "key1": "value1", "key2": "value2" }
-                    ]
-                },
-                "key6":
+                "key":
                   [
-                    { "key1": "value1", "key": "value" },
-                    { "key1": "value1", "key": "value" },
-                    { "key1": "value1", "key": "value" },
-                    { "key1": "value1", "key": "value" },
-                    { "key1": "value1", "key": "value" },
-                    { "key1": "value1", "key": "value" },
-                    { "key1": "value1", "key": "value" }
+                    { "key1": "value1", "key2": "value2" },
+                    { "key1": "value1", "key2": "value2" },
+                    { "key1": "value1", "key2": "value2" },
+                    { "key1": "value1", "key2": "value2" }
                   ]
               },
-            ]
-        }
-      };
-
-      for (var i = 0; i < sourceRowCount; i++)
-      {
-        arrJSON.push(sampleJSON);
+              "key6":
+                [
+                  { "key1": "value1", "key": "value" },
+                  { "key1": "value1", "key": "value" },
+                  { "key1": "value1", "key": "value" },
+                  { "key1": "value1", "key": "value" },
+                  { "key1": "value1", "key": "value" },
+                  { "key1": "value1", "key": "value" },
+                  { "key1": "value1", "key": "value" }
+                ]
+            },
+          ]
       }
+    };
 
-      async.series([
-        function (callback)
-        {
-          connection.execute({
-            sqlText: insertVariant,
-            binds: [JSON.stringify(arrJSON)],
-            complete: function (err, stmt)
+    for (var i = 0; i < sourceRowCount; i++)
+    {
+      arrJSON.push(sampleJSON);
+    }
+
+    async.series([
+      function (callback)
+      {
+        testUtil.executeCmd(connection, createTableWithVariant, callback);
+      },
+      function (callback)
+      {
+        connection.execute({
+          sqlText: insertVariant,
+          binds: [JSON.stringify(arrJSON)],
+          complete: function (err, stmt)
+          {
+            if (err)
             {
-              if (err)
+              callback(err);
+            }
+            else
+            {
+              try
+              {
+                assert.strictEqual(stmt.getNumUpdatedRows(), sourceRowCount);
+                callback();
+              }
+              catch (err)
               {
                 callback(err);
               }
-              else
+            }
+          }
+        });
+      },
+      function (callback)
+      {
+        connection.execute({
+          sqlText: selectVariant,
+          streamResult: true,
+          complete: function (err, stmt)
+          {
+            if (err)
+            {
+              callback(err);
+            }
+            else
+            {
+              var stream = stmt.streamRows();
+              var rowCount = 0;
+              stream.on('data', function ()
+              {
+                rowCount++;
+              });
+              stream.on('error', function (err)
+              {
+                callback(err);
+              });
+              stream.on('end', function ()
               {
                 try
                 {
-                  assert.strictEqual(stmt.getNumUpdatedRows(), sourceRowCount);
+                  assert.strictEqual(rowCount, sourceRowCount);
                   callback();
                 }
                 catch (err)
                 {
                   callback(err);
                 }
-              }
+              });
             }
-          });
-        },
-        function (callback)
-        {
-          connection.execute({
-            sqlText: selectVariant,
-            streamResult: true,
-            complete: function (err, stmt)
-            {
-              if (err)
-              {
-                callback(err);
-              }
-              else
-              {
-                var stream = stmt.streamRows();
-                var rowCount = 0;
-                stream.on('data', function ()
-                {
-                  rowCount++;
-                });
-                stream.on('error', function (err)
-                {
-                  callback(err);
-                });
-                stream.on('end', function ()
-                {
-                  try
-                  {
-                    assert.strictEqual(rowCount, sourceRowCount);
-                    callback();
-                  }
-                  catch (err)
-                  {
-                    callback(err);
-                  }
-                });
-              }
-            }
-          });
-        }],
-        done
-      );
-    });
+          }
+        });
+      },
+      function (callback)
+      {
+        testUtil.executeCmd(connection, dropTableWithVariant, callback);
+      }],
+      done
+    );
   });
 });

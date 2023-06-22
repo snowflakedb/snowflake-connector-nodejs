e2b3676c4bf68c4b2c50c593d42ad1e0d4f6d598
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2023-06-22 14:43:01 -0700

    Wrap variant test cases in their own describe

--- test/integration/testDataType.js
+++ test/integration/testDataType.js
@@ -28,6 +28,7 @@ describe('Test DataType', function ()
   var dropTableWithTime = 'drop table if exists testTime';
   var dropTableWithTimestamp = 'drop table if exists testTimestamp';
   var dropTableWithBoolean = 'drop table if exists testBoolean';
+  const truncateTableWithVariant = 'truncate table if exists testVariant;'
   var insertDouble = 'insert into testDouble values(123.456)';
   var insertLargeNumber = 'insert into testNumber values (12345678901234567890123456789012345678)';
   var insertRegularSizedNumber = 'insert into testNumber values (100000001)';
@@ -217,60 +218,70 @@ describe('Test DataType', function ()
 
   describe('testSemiStructuredDataType', function ()
   {
-    it('testVariantJSON', function (done)
+    describe('testVariant', function ()
     {
-      async.series(
-        [
-          function (callback)
-          {
-            testUtil.executeCmd(connection, createTableWithVariant, callback);
-          },
-          function (callback)
-          {
-            testUtil.executeCmd(connection, insertVariantJSON, callback);
-          },
-          function (callback)
-          {
-            testUtil.executeQueryAndVerify(
-              connection,
-              selectVariant,
-              [{'COLA': {a: 1, b: [1, 2, 3, -Infinity, undefined], c: {a: 1}}}],
-              callback,
-              null,
-              true,
-              false
-            );
-          }],
-        done
-      );
-    });
+      before(async () =>
+      {
+        await testUtil.executeCmdAsync(connection, createTableWithVariant);
+      });
 
-    it('testVariantXML', function (done)
-    {
-      async.series(
-        [
-          function (callback)
-          {
-            testUtil.executeCmd(connection, createTableWithVariant, callback);
-          },
-          function (callback)
-          {
-            testUtil.executeCmd(connection, insertVariantXML, callback);
-          },
-          function (callback)
-          {
-            testUtil.executeQueryAndVerify(
-              connection,
-              selectVariant,
-              [{ 'COLA': { root: { a: 1, b: 1, c: { a: 1 } } }}],
-              callback,
-              null,
-              true,
-              false
-            );
-          }],
-        done
-      );
+      after(async () =>
+      {
+        await testUtil.executeCmdAsync(connection, dropTableWithVariant);
+      });
+
+      afterEach(async () =>
+      {
+        await testUtil.executeCmdAsync(connection, truncateTableWithVariant);
+      });
+
+      it('testJSON', function (done)
+      {
+        async.series(
+          [
+            function (callback)
+            {
+              testUtil.executeCmd(connection, insertVariantJSON, callback);
+            },
+            function (callback)
+            {
+              testUtil.executeQueryAndVerify(
+                connection,
+                selectVariant,
+                [{ 'COLA': { a: 1, b: [1, 2, 3, -Infinity, undefined], c: { a: 1 } } }],
+                callback,
+                null,
+                true,
+                false
+              );
+            }],
+          done
+        );
+      });
+
+      it('testXML', function (done)
+      {
+        async.series(
+          [
+            function (callback)
+            {
+              testUtil.executeCmd(connection, insertVariantXML, callback);
+            },
+            function (callback)
+            {
+              testUtil.executeQueryAndVerify(
+                connection,
+                selectVariant,
+                [{ 'COLA': { root: { a: 1, b: 1, c: { a: 1 } } } }],
+                callback,
+                null,
+                true,
+                false
+              );
+            }],
+          done
+        );
+      });
     });
 
     it('testArray', function (done)

29e122a72714ad05617e203a64dd805245dc13e3
sfc-gh-ext-simba-lf <lfarol@magnitude.com>
2023-06-22 14:55:17 -0700

    issue 366 - Wrap the configure test cases in their own describe

--- test/unit/snowflake_config_test.js
+++ test/unit/snowflake_config_test.js
@@ -27,167 +27,164 @@ describe('Snowflake Configure Tests', function () {
     snowflake.configure(originalConfig);
   });
 
-  ///////////////////////////////////////////////////////////////////////////
-  //// Test invalid arguments                                            ////
-  ///////////////////////////////////////////////////////////////////////////
+  describe('Test invalid arguments', function () {
+    const negativeTestCases =
+      [
+        {
+          name: 'invalid logLevel',
+          options: { logLevel: 'unsupported' },
+          errorCode: ErrorCodes.ERR_GLOBAL_CONFIGURE_INVALID_LOG_LEVEL
+        },
+        {
+          name: 'invalid insecureConnect',
+          options: { insecureConnect: 'unsupported' },
+          errorCode: ErrorCodes.ERR_GLOBAL_CONFIGURE_INVALID_INSECURE_CONNECT
+        },
+        {
+          name: 'invalid ocspMode',
+          options: { ocspFailOpen: 'unsupported' },
+          errorCode: ErrorCodes.ERR_GLOBAL_CONFIGURE_INVALID_OCSP_MODE
+        },
+        {
+          name: 'invalid json parser',
+          options: { jsonColumnVariantParser: 'unsupported' },
+          errorCode: ErrorCodes.ERR_GLOBAL_CONFIGURE_INVALID_JSON_PARSER
+        },
+        {
+          name: 'invalid xml parser',
+          options: { xmlColumnVariantParser: 'unsupported' },
+          errorCode: ErrorCodes.ERR_GLOBAL_CONFIGURE_INVALID_XML_PARSER
+        },
+      ];
 
-  const negativeTestCases =
-    [
-      {
-        name: 'invalid logLevel',
-        options: { logLevel: 'unsupported' },
-        errorCode: ErrorCodes.ERR_GLOBAL_CONFIGURE_INVALID_LOG_LEVEL
-      },
-      {
-        name: 'invalid insecureConnect',
-        options: { insecureConnect: 'unsupported' },
-        errorCode: ErrorCodes.ERR_GLOBAL_CONFIGURE_INVALID_INSECURE_CONNECT
-      },
-      {
-        name: 'invalid ocspMode',
-        options: { ocspFailOpen: 'unsupported' },
-        errorCode: ErrorCodes.ERR_GLOBAL_CONFIGURE_INVALID_OCSP_MODE
-      },
-      {
-        name: 'invalid json parser',
-        options: { jsonColumnVariantParser: 'unsupported' },
-        errorCode: ErrorCodes.ERR_GLOBAL_CONFIGURE_INVALID_JSON_PARSER
-      },
-      {
-        name: 'invalid xml parser',
-        options: { xmlColumnVariantParser: 'unsupported' },
-        errorCode: ErrorCodes.ERR_GLOBAL_CONFIGURE_INVALID_XML_PARSER
-      },
-    ];
+    const createNegativeITCallback = function (testCase) {
+      return function () {
+        let error;
 
-  const createNegativeITCallback = function (testCase) {
-    return function () {
-      let error;
+        try {
+          snowflake.configure(testCase.options);
+        } catch (err) {
+          error = err;
+        } finally {
+          assert.ok(error);
+          assert.strictEqual(error.code, testCase.errorCode);
+        }
+      };
+    };
 
-      try {
+    let index, length, testCase;
+    for (index = 0, length = negativeTestCases.length; index < length; index++) {
+      testCase = negativeTestCases[index];
+      it(testCase.name, createNegativeITCallback(testCase));
+    }
+  });
+
+  describe('Test valid arguments', function () {
+    const testCases =
+      [
+        {
+          name: 'logLevel error',
+          options:
+          {
+            logLevel: LOG_LEVEL_TAGS.ERROR
+          }
+        },
+        {
+          name: 'logLevel warn',
+          options:
+          {
+            logLevel: LOG_LEVEL_TAGS.WARN
+          }
+        },
+        {
+          name: 'logLevel debug',
+          options:
+          {
+            logLevel: LOG_LEVEL_TAGS.DEBUG
+          }
+        },
+        {
+          name: 'logLevel info',
+          options:
+          {
+            logLevel: LOG_LEVEL_TAGS.INFO
+          }
+        },
+        {
+          name: 'logLevel trace',
+          options:
+          {
+            logLevel: LOG_LEVEL_TAGS.TRACE
+          }
+        },
+        {
+          name: 'insecureConnect false',
+          options:
+          {
+            insecureConnect: false
+          }
+        },
+        {
+          name: 'insecureConnect true',
+          options:
+          {
+            insecureConnect: true
+          }
+        },
+        {
+          name: 'ocspFailOpen false',
+          options:
+          {
+            ocspFailOpen: false
+          }
+        },
+        {
+          name: 'ocspFailOpen true',
+          options:
+          {
+            ocspFailOpen: true
+          }
+        },
+        {
+          name: 'json parser',
+          options:
+          {
+            jsonColumnVariantParser: rawColumnValue => require('vm').runInNewContext('(' + rawColumnValue + ')')
+          }
+        },
+        {
+          name: 'xml parser',
+          options:
+          {
+            xmlColumnVariantParser: rawColumnValue => (require('fast-xml-parser').XMLParser()).parse(rawColumnValue)
+          }
+        },
+      ];
+
+    const createItCallback = function (testCase) {
+      return function () {
         snowflake.configure(testCase.options);
-      } catch (err) {
-        error = err;
-      } finally {
-        assert.ok(error);
-        assert.strictEqual(error.code, testCase.errorCode);
-      }
+        Object.keys(testCase.options).forEach(function (key) {
+          const ref = testCase.options[key];
+          let val;
+          if (key == 'logLevel') {
+            val = Logger.getInstance().getLevelTag();
+          } else if (key == 'insecureConnect') {
+            val = GlobalConfig.isInsecureConnect();
+          } else if (key == 'ocspFailOpen') {
+            val = GlobalConfig.getOcspFailOpen();
+          } else {
+            val = GlobalConfig[key];
+          }
+          assert.strictEqual(val, ref);
+        });
+      };
     };
-  };
 
-  let index, length, testCase;
-  for (index = 0, length = negativeTestCases.length; index < length; index++) {
-    testCase = negativeTestCases[index];
-    it(testCase.name, createNegativeITCallback(testCase));
-  }
-
-  ///////////////////////////////////////////////////////////////////////////
-  //// Test valid arguments                                              ////
-  ///////////////////////////////////////////////////////////////////////////
-
-  const testCases =
-    [
-      {
-        name: 'logLevel error',
-        options:
-        {
-          logLevel: LOG_LEVEL_TAGS.ERROR
-        }
-      },
-      {
-        name: 'logLevel warn',
-        options:
-        {
-          logLevel: LOG_LEVEL_TAGS.WARN
-        }
-      },
-      {
-        name: 'logLevel debug',
-        options:
-        {
-          logLevel: LOG_LEVEL_TAGS.DEBUG
-        }
-      },
-      {
-        name: 'logLevel info',
-        options:
-        {
-          logLevel: LOG_LEVEL_TAGS.INFO
-        }
-      },
-      {
-        name: 'logLevel trace',
-        options:
-        {
-          logLevel: LOG_LEVEL_TAGS.TRACE
-        }
-      },
-      {
-        name: 'insecureConnect false',
-        options:
-        {
-          insecureConnect: false
-        }
-      },
-      {
-        name: 'insecureConnect true',
-        options:
-        {
-          insecureConnect: true
-        }
-      },
-      {
-        name: 'ocspFailOpen false',
-        options:
-        {
-          ocspFailOpen: false
-        }
-      },
-      {
-        name: 'ocspFailOpen true',
-        options:
-        {
-          ocspFailOpen: true
-        }
-      },
-      {
-        name: 'json parser',
-        options:
-        {
-          jsonColumnVariantParser: rawColumnValue => require('vm').runInNewContext('(' + rawColumnValue + ')')
-        }
-      },
-      {
-        name: 'xml parser',
-        options:
-        {
-          xmlColumnVariantParser: rawColumnValue => (require('fast-xml-parser').XMLParser()).parse(rawColumnValue)
-        }
-      },
-    ];
-
-  const createItCallback = function (testCase) {
-    return function () {
-      snowflake.configure(testCase.options);
-      Object.keys(testCase.options).forEach(function (key) {
-        const ref = testCase.options[key];
-        let val;
-        if (key == 'logLevel') {
-          val = Logger.getInstance().getLevelTag();
-        } else if (key == 'insecureConnect') {
-          val = GlobalConfig.isInsecureConnect();
-        } else if (key == 'ocspFailOpen') {
-          val = GlobalConfig.getOcspFailOpen();
-        } else {
-          val = GlobalConfig[key];
-        }
-        assert.strictEqual(val, ref);
-      });
-    };
-  };
-
-  for (index = 0, length = testCases.length; index < length; index++) {
-    testCase = testCases[index];
-    it(testCase.name, createItCallback(testCase));
-  }
+    let index, length, testCase;
+    for (index = 0, length = testCases.length; index < length; index++) {
+      testCase = testCases[index];
+      it(testCase.name, createItCallback(testCase));
+    }
+  });
 });

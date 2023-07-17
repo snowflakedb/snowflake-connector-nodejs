8ff508adaa419a92ff4df61f36bee623a6e58df1
Dominik Przybysz <dominik.przybysz@snowflake.com>
2023-07-17 09:27:18 +0200

    SNOW-847264: Use node 12 in linux tests

--- ci/container/test_component.sh
+++ ci/container/test_component.sh
@@ -24,6 +24,29 @@ if [[ "$LOCAL_USER_NAME" == "jenkins" ]]; then
 else
     export PATH=$WORKSPACE/node_modules/mocha/bin:$PATH
 fi
+
+echo "[INFO] Build is using node versions"
+npm version
+
+set +e
+npm version | grep "node: '12"
+usingNode12=$?
+set -e
+if [ $usingNode12 -eq 0 ]
+then
+  echo "[DEBUG] We are using node v12"
+else
+  echo "[DEBUG] Installing newer node 12"
+  export NVM_DIR=`pwd`/nvm
+  cp -r /usr/local/nvm $NVM_DIR
+  source $NVM_DIR/nvm.sh
+  nvm install 12
+
+  echo "[INFO] Build is using node versions"
+  npm version
+fi
+
+echo "[INFO] Installing"
 cp $SOURCE_ROOT/ci/container/package.json .
 npm install
 

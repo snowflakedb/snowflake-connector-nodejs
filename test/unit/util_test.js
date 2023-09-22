bf56e2ec45d0bbb525409ab607d50419f8180646
sfc-gh-ext-simba-jy <jonghun.yun@insightsoftware.com>
2023-09-22 00:52:33 -0700

    fixed wrong codes and revised test naming

--- test/unit/util_test.js
+++ test/unit/util_test.js
@@ -488,34 +488,37 @@ describe('Util', function ()
     }
   });
 
-  it('Util.url.appendRetryParam()', function () {
-    let url;
+  describe('Append retry parameters', function () {
     const testCases =
       [
         {
+          testName: "test appending retry params with retry reason",
           option: {
             url: 'http://www.something.snowflakecomputing.com',
             retryCount: 3,
             retryReason: 429,
-            includeRetryContext: true,
+            includeRetryReason: true,
           },
           result: 'http://www.something.snowflakecomputing.com?retryCount=3&retryReason=429'
         },
         {
+          testName: "test appending retry params without retry reason",
           option: {
             url: 'http://www.something.snowflakecomputing.com',
             retryCount: 3,
             retryReason: 429,
-            includeRetryContext: false,
+            includeRetryReason: false,
           },
           result: 'http://www.something.snowflakecomputing.com?retryCount=3'
         }
       ];
 
       for (let i = 0; i < testCases.length; i++) {
-        testCase = testCases[i];
-        url = Util.url.appendRetryParam(testCase.option);
-        assert.strictEqual(url, testCase.result);
+        const testCase = testCases[i];
+        it(testCase.testName, function () {
+          const url = Util.url.appendRetryParam(testCase.option);
+          assert.strictEqual(url, testCase.result);
+        })
       }
   })
 

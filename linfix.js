c0482163cfa2f96581dbee375a1fac49c51eebb6
John Yun <140559986+sfc-gh-ext-simba-jy@users.noreply.github.com>
2023-12-15 12:30:02 -0800

    Delete linfix.js

--- linfix.js
+++
@@ -1,7 +0,0 @@
-let retryTimeout = 300;
-if (Util.exists(options.retryTimeout)) {
-  Errors.checkArgumentValid(Util.isNumber(options.retryTimeout),
-    ErrorCodes.ERR_CONN_CREATE_INVALID_MAX_RETRY_TIMEOUT);
-
-  retryTimeout = options.retryTimeout !== 0 ? Math.max(retryTimeout, options.retryTimeout) : 0;
-}

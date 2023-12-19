7a69a8418a36dd96b5056b7f4e842afbb9c3e096
Gabriel Lomba <glombaaguiarcosta@gmail.com>
2023-12-19 15:01:57 +0200

    Destroy clients on a finally clause + improve client destruction API

--- test/unit/file_transfer_agent/s3_test.js
+++ test/unit/file_transfer_agent/s3_test.js
@@ -81,7 +81,7 @@ describe('S3 client', function () {
     };
   });
   this.afterEach(function () {
-    meta['client'].destroy();
+    AWS.destroyClient(meta['client']);
   });
 
   it('extract bucket name and path', async function () {

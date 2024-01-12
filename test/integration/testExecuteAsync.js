cc5c392c487ec9f27003b0580907b0887a194709
sfc-gh-ext-simba-jy <jonghun.yun@insightsoftware.com>
2024-01-12 11:15:15 -0800

    fixed testing cases

--- test/integration/testExecuteAsync.js
+++ test/integration/testExecuteAsync.js
@@ -137,7 +137,7 @@ describe('ExecuteAsync test', function () {
             await connection.getQueryStatusThrowIfError(queryId);
             assert.fail();
           } catch (err) {
-            assert.ok(err);
+            assert.ok(err.code !== 'ERR_ASSERTION');
           }
 
           // Check getting the results throws an error
@@ -145,7 +145,8 @@ describe('ExecuteAsync test', function () {
             await connection.getResultsFromQueryId({ queryId: queryId });
             assert.fail();
           } catch (err) {
-            assert.ok(err);
+            assert.ok(err.code !== 'ERR_ASSERTION');
+
           }
         }
       ],

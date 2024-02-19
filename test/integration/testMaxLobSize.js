856462b652bef9cd08128936ae84c47c107f4770
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2024-02-19 15:32:47 +0100

    fix lint errors

--- test/integration/testMaxLobSize.js
+++ test/integration/testMaxLobSize.js
@@ -12,7 +12,7 @@ function generateRandomString(sizeInBytes) {
 }
 
 if (process.env.RUN_MANUAL_TESTS_ONLY === 'true'){
-  describe.only('Max LOB test', function () {
+  describe.only('Max LOB test 2', function () {
     let connection;
     // This size cannot be tested on our env. The snowflake team should test internally.
     const MAX_LOB_SIZE = 128 * 1024 * 1024; 

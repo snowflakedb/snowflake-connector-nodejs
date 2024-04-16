269729792218a42346f8c09fa7eaca65b260b353
Luis Montero Marin <luis.monteromarin@snowflake.com>
2024-04-16 09:46:31 -0600

    Fix error related to milliseconds precision

--- test/unit/connection/result/result_test_timestamp.js
+++ test/unit/connection/result/result_test_timestamp.js
@@ -7,6 +7,7 @@ const assert = require('assert');
 const ResultTestCommon = require('./result_test_common');
 
 describe('Result: test timestamp', function () {
+
   it('select to_timestamp_ltz(\'Thu, 21 Jan 2016 06:32:44 -0800\') as C1, ' +
     'to_timestamp_tz(\'Thu, 21 Jan 2016 06:32:44 -0800\') as C2, ' +
     'to_timestamp_ntz(\'Thu, 21 Jan 2016 06:32:44 -0800\') as C3;',
@@ -140,5 +141,238 @@ describe('Result: test timestamp', function () {
           done();
         }
       );
-    });
+    }
+  );
+
+  it('select dateadd(ms,999, to_timestamp_ntz(\'2024-04-16T14:57:58\', \'YYYY-MM-DD"T"HH24:MI:SS\')) AS C1;',
+    function (done) {
+      const response =
+        {
+          'data': {
+            'parameters': [
+              { 'name': 'TIMEZONE', 'value': 'America/Los_Angeles' },
+              { 'name': 'TIMESTAMP_OUTPUT_FORMAT', 'value': 'YYYY-MM-DD HH24:MI:SS.FF3' },
+              { 'name': 'TIMESTAMP_NTZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'TIMESTAMP_LTZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'TIMESTAMP_TZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'DATE_OUTPUT_FORMAT', 'value': 'YYYY-MM-DD' },
+              { 'name': 'CLIENT_RESULT_PREFETCH_SLOTS', 'value': 2 },
+              { 'name': 'CLIENT_RESULT_PREFETCH_THREADS', 'value': 1 },
+              { 'name': 'CLIENT_HONOR_CLIENT_TZ_FOR_TIMESTAMP_NTZ', 'value': true },
+              { 'name': 'CLIENT_USE_V1_QUERY_API', 'value': true }
+            ],
+            'rowtype': [{
+              'name': 'C1',
+              'byteLength': null,
+              'nullable': false,
+              'precision': 0,
+              'scale': 9,
+              'length': null,
+              'type': 'timestamp_ntz'
+            }],
+            'rowset': [['1713279478.999']],
+            'total': 1,
+            'returned': 1,
+            'queryId': 'b603b7fb-48df-48b6-bc90-25a7cb355e1d',
+            'databaseProvider': null,
+            'finalDatabaseName': null,
+            'finalSchemaName': null,
+            'finalWarehouseName': 'NEW_WH',
+            'finalRoleName': 'ACCOUNTADMIN',
+            'numberOfBinds': 0,
+            'statementTypeId': 4096,
+            'version': 0
+          },
+          'message': null,
+          'code': null,
+          'success': true
+        };
+
+      ResultTestCommon.testResult(
+        ResultTestCommon.createResultOptions(response),
+        function (row) {
+          const actualTimestamp = row.getColumnValue('C1');
+          assert.ok(Util.isDate(row.getColumnValue('C1')));
+          assert.strictEqual(actualTimestamp.toJSON(), '2024-04-16 14:57:58.999');
+        },
+        function () {
+          done();
+        }
+      );
+    }
+  );
+
+  it('select dateadd(ms,1, to_timestamp_ntz(\'2024-04-16T14:57:58\', \'YYYY-MM-DD"T"HH24:MI:SS\')) AS C1;',
+    function (done) {
+      const response =
+        {
+          'data': {
+            'parameters': [
+              { 'name': 'TIMEZONE', 'value': 'America/Los_Angeles' },
+              { 'name': 'TIMESTAMP_OUTPUT_FORMAT', 'value': 'YYYY-MM-DD HH24:MI:SS.FF3' },
+              { 'name': 'TIMESTAMP_NTZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'TIMESTAMP_LTZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'TIMESTAMP_TZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'DATE_OUTPUT_FORMAT', 'value': 'YYYY-MM-DD' },
+              { 'name': 'CLIENT_RESULT_PREFETCH_SLOTS', 'value': 2 },
+              { 'name': 'CLIENT_RESULT_PREFETCH_THREADS', 'value': 1 },
+              { 'name': 'CLIENT_HONOR_CLIENT_TZ_FOR_TIMESTAMP_NTZ', 'value': true },
+              { 'name': 'CLIENT_USE_V1_QUERY_API', 'value': true }
+            ],
+            'rowtype': [{
+              'name': 'C1',
+              'byteLength': null,
+              'nullable': false,
+              'precision': 0,
+              'scale': 9,
+              'length': null,
+              'type': 'timestamp_ntz'
+            }],
+            'rowset': [['1713279478.001']],
+            'total': 1,
+            'returned': 1,
+            'queryId': 'b603b7fb-48df-48b6-bc90-25a7cb355e1d',
+            'databaseProvider': null,
+            'finalDatabaseName': null,
+            'finalSchemaName': null,
+            'finalWarehouseName': 'NEW_WH',
+            'finalRoleName': 'ACCOUNTADMIN',
+            'numberOfBinds': 0,
+            'statementTypeId': 4096,
+            'version': 0
+          },
+          'message': null,
+          'code': null,
+          'success': true
+        };
+
+      ResultTestCommon.testResult(
+        ResultTestCommon.createResultOptions(response),
+        function (row) {
+          const actualTimestamp = row.getColumnValue('C1');
+          assert.ok(Util.isDate(row.getColumnValue('C1')));
+          assert.strictEqual(actualTimestamp.toJSON(), '2024-04-16 14:57:58.001');
+        },
+        function () {
+          done();
+        }
+      );
+    }
+  );
+  
+  it('select dateadd(ns,999999999, to_timestamp_ntz(\'2024-04-16T14:57:58\', \'YYYY-MM-DD"T"HH24:MI:SS\')) AS C1;',
+    function (done) {
+      const response =
+        {
+          'data': {
+            'parameters': [
+              { 'name': 'TIMEZONE', 'value': 'America/Los_Angeles' },
+              { 'name': 'TIMESTAMP_OUTPUT_FORMAT', 'value': 'YYYY-MM-DD HH24:MI:SS.FF9' },
+              { 'name': 'TIMESTAMP_NTZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'TIMESTAMP_LTZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'TIMESTAMP_TZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'DATE_OUTPUT_FORMAT', 'value': 'YYYY-MM-DD' },
+              { 'name': 'CLIENT_RESULT_PREFETCH_SLOTS', 'value': 2 },
+              { 'name': 'CLIENT_RESULT_PREFETCH_THREADS', 'value': 1 },
+              { 'name': 'CLIENT_HONOR_CLIENT_TZ_FOR_TIMESTAMP_NTZ', 'value': true },
+              { 'name': 'CLIENT_USE_V1_QUERY_API', 'value': true }
+            ],
+            'rowtype': [{
+              'name': 'C1',
+              'byteLength': null,
+              'nullable': false,
+              'precision': 0,
+              'scale': 9,
+              'length': null,
+              'type': 'timestamp_ntz'
+            }],
+            'rowset': [['1713279478.999999999']],
+            'total': 1,
+            'returned': 1,
+            'queryId': 'b603b7fb-48df-48b6-bc90-25a7cb355e1d',
+            'databaseProvider': null,
+            'finalDatabaseName': null,
+            'finalSchemaName': null,
+            'finalWarehouseName': 'NEW_WH',
+            'finalRoleName': 'ACCOUNTADMIN',
+            'numberOfBinds': 0,
+            'statementTypeId': 4096,
+            'version': 0
+          },
+          'message': null,
+          'code': null,
+          'success': true
+        };
+
+      ResultTestCommon.testResult(
+        ResultTestCommon.createResultOptions(response),
+        function (row) {
+          const actualTimestamp = row.getColumnValue('C1');
+          assert.ok(Util.isDate(row.getColumnValue('C1')));
+          assert.strictEqual(actualTimestamp.toJSON(), '2024-04-16 14:57:58.999999999');
+        },
+        function () {
+          done();
+        }
+      );
+    }
+  );
+
+  it('select dateadd(ns,1, to_timestamp_ntz(\'2024-04-16T14:57:58\', \'YYYY-MM-DD"T"HH24:MI:SS\')) AS C1;',
+    function (done) {
+      const response =
+        {
+          'data': {
+            'parameters': [
+              { 'name': 'TIMEZONE', 'value': 'America/Los_Angeles' },
+              { 'name': 'TIMESTAMP_OUTPUT_FORMAT', 'value': 'YYYY-MM-DD HH24:MI:SS.FF9' },
+              { 'name': 'TIMESTAMP_NTZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'TIMESTAMP_LTZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'TIMESTAMP_TZ_OUTPUT_FORMAT', 'value': '' },
+              { 'name': 'DATE_OUTPUT_FORMAT', 'value': 'YYYY-MM-DD' },
+              { 'name': 'CLIENT_RESULT_PREFETCH_SLOTS', 'value': 2 },
+              { 'name': 'CLIENT_RESULT_PREFETCH_THREADS', 'value': 1 },
+              { 'name': 'CLIENT_HONOR_CLIENT_TZ_FOR_TIMESTAMP_NTZ', 'value': true },
+              { 'name': 'CLIENT_USE_V1_QUERY_API', 'value': true }
+            ],
+            'rowtype': [{
+              'name': 'C1',
+              'byteLength': null,
+              'nullable': false,
+              'precision': 0,
+              'scale': 9,
+              'length': null,
+              'type': 'timestamp_ntz'
+            }],
+            'rowset': [['1713279478.000000001']],
+            'total': 1,
+            'returned': 1,
+            'queryId': 'b603b7fb-48df-48b6-bc90-25a7cb355e1d',
+            'databaseProvider': null,
+            'finalDatabaseName': null,
+            'finalSchemaName': null,
+            'finalWarehouseName': 'NEW_WH',
+            'finalRoleName': 'ACCOUNTADMIN',
+            'numberOfBinds': 0,
+            'statementTypeId': 4096,
+            'version': 0
+          },
+          'message': null,
+          'code': null,
+          'success': true
+        };
+
+      ResultTestCommon.testResult(
+        ResultTestCommon.createResultOptions(response),
+        function (row) {
+          const actualTimestamp = row.getColumnValue('C1');
+          assert.ok(Util.isDate(row.getColumnValue('C1')));
+          assert.strictEqual(actualTimestamp.toJSON(), '2024-04-16 14:57:58.000000001');
+        },
+        function () {
+          done();
+        }
+      );
+    }
+  );
 });

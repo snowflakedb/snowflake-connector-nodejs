0745e5347f6480c4060fd185dcee73da4363d73f
sfc-gh-ext-simba-jy <jonghun.yun@insightsoftware.com>
2024-04-24 21:32:38 -0700

    refactored the json credential manager

--- test/unit/authentication/json_credential_manager_test.js
+++ test/unit/authentication/json_credential_manager_test.js
@@ -19,51 +19,31 @@ if (!(currentNodeVersion <= 14 && (os.platform() === 'win32'))) {
   describe('Json credential manager test', function () {
     const credentialManager = new JsonCredentialManager();
 
-    it('test - initiate credential manager', function () {
-      credentialManager.remove(key);
-      const savedPassword = credentialManager.read(key);
+    it('test - initiate credential manager', async function () {
+      if(await credentialManager.read(key) !== null) {
+        await credentialManager.remove(key);
+      }
+      const savedPassword = await credentialManager.read(key);
     
-      assert.strictEqual(credentialManager.tokenDir, path.join(os.homedir(), 'temporary_credential.json'));
+      assert.strictEqual(await credentialManager.getTokenDir(), path.join(os.homedir(), 'temporary_credential.json'));
       assert.strictEqual(savedPassword, null);
     });
 
-    it('test - write the mock credential with the credential manager', function () {
-      credentialManager.write(key, randomPassword);
-      const result = credentialManager.read(key);
+    it('test - write the mock credential with the credential manager', async function () {
+      await credentialManager.write(key, randomPassword);
+      const result = await credentialManager.read(key);
       assert.strictEqual(randomPassword, result);
     });
 
-    it('test - delete the mock credential with the credential manager', function () {
-      credentialManager.remove(key);
-      const result = credentialManager.read(key);
+    it('test - delete the mock credential with the credential manager', async function () {
+      await credentialManager.remove(key);
+      const result = await credentialManager.read(key);
       assert.ok(result === null);
     });
 
-    it('test - token saving location when the user sets credentialCacheDir value', function () {
+    it('test - token saving location when the user sets credentialCacheDir value', async function () {
       const credManager = new JsonCredentialManager(os.tmpdir());
-      assert.strictEqual(credManager.tokenDir, path.join(os.tmpdir(), 'temporary_credential.json'));
-    });
-
-  });
-
-  describe('Json credential manager - no valid saving location', function () {
-    const credentialManager = new JsonCredentialManager();
-    credentialManager.tokenDir = null;
-
-    it('test - initial the credential manager', function () {
-      assert.strictEqual(credentialManager.tokenDir, null);
-    });
-
-    it('test - read the mock credential with the credential manager', function () {
-      assert.strictEqual(credentialManager.read(key), null);
-    });
-
-    it('test - write the mock credential with the credential manager', function () {
-      assert.strictEqual(credentialManager.write(key, randomPassword), null);
-    });
-
-    it('test - delete the mock credential with the credential manager', function () {
-      assert.strictEqual(credentialManager.remove(key), null);
+      assert.strictEqual(await credManager.getTokenDir(), path.join(os.tmpdir(), 'temporary_credential.json'));
     });
   });
 }

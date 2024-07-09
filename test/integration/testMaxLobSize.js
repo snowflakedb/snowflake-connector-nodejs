3157e504854bc6b2295f95d49c38b99b450de7e9
Dawid Heyman <dawid.heyman@snowflake.com>
2024-07-09 15:35:27 +0200

    SNOW-1163210 Add test for large values stage insert

--- test/integration/testMaxLobSize.js
+++ test/integration/testMaxLobSize.js
@@ -8,6 +8,11 @@ const testUtil = require('./testUtil');
 const crypto = require('crypto');
 const snowflake = require('./../../lib/snowflake');
 const connOption = require('./connectionOptions');
+const os = require('os');
+const path = require('path');
+
+const DATABASE_NAME = connOption.valid.database;
+const SCHEMA_NAME = connOption.valid.schema;
 
 function generateRandomString(sizeInBytes) {
   const bufferSize = Math.ceil(sizeInBytes / 2);
@@ -21,63 +26,61 @@ if (process.env.RUN_MANUAL_TESTS_ONLY === 'true'){
     // This size cannot be tested on our env. The snowflake team should test internally.
     const MAX_LOB_SIZE = 128 * 1024 * 1024;
     const LARGE_SIZE = MAX_LOB_SIZE / 2;
-    const MEDIEUM_SIZE = LARGE_SIZE / 2;
-    const ORIGIN_SIZE = MEDIEUM_SIZE / 2;
+    const MEDIUM_SIZE = LARGE_SIZE / 2;
+    const ORIGIN_SIZE = MEDIUM_SIZE / 2;
     const SMALL_SIZE = 16;
 
     const tableName = 'my_lob_test';
     const createTable = `create or replace table ${tableName} (c1 varchar, c2 varchar, c3 int)`;
+    const allowLobExternalScan = 'alter session set ALLOW_LARGE_LOBS_IN_EXTERNAL_SCAN = true';
+    const stageName = `@${DATABASE_NAME}.${SCHEMA_NAME}.%${tableName}`;
+    const copyIntoTable = `copy into ${tableName}`;
+    const removeStage = `remove ${stageName}`;
     const normalInsert = `insert into ${tableName}(c1, c2, c3) values `;
     const positionalBindingInsert = `insert into ${tableName}(c1, c2, c3) values (?, ?, ?)`;
     const namedBindingInsert = `insert into ${tableName}(c1, c2, c3) values (:1, :2, :3)`;
     const selectTable = `select * FROM ${tableName}`;
 
-    const testCases = [
-      {
-        name: 'test - insert 16byte size data',
-        data: {
-          C1: generateRandomString(SMALL_SIZE),
-          C2: generateRandomString(SMALL_SIZE),
-          C3: Math.ceil(Math.random() * 100000)
-        },
+    const smallSizeTableData = {
+      C1: generateRandomString(SMALL_SIZE),
+      C2: generateRandomString(SMALL_SIZE),
+      C3: Math.ceil(Math.random() * 100000)
+    };
 
-      },
-      {
-        name: 'test - insert 16MB size data',
-        data: {
-          C1: generateRandomString(ORIGIN_SIZE),
-          C2: generateRandomString(ORIGIN_SIZE),
-          C3: Math.ceil(Math.random() * 100000)
-        },
-      },
-      {
-        name: 'test - insert 32MB size data',
-        data: {
-          C1: generateRandomString(MEDIEUM_SIZE),
-          C2: generateRandomString(ORIGIN_SIZE),
-          C3: Math.ceil(Math.random() * 100000)
-        },
-      },
-      {
-        name: 'test - insert 64MB size data',
-        data: {
-          C1: generateRandomString(LARGE_SIZE),
-          C2: generateRandomString(ORIGIN_SIZE),
-          C3: Math.ceil(Math.random() * 100000)
-        },
-      },
-      {
-        name: 'test - insert 128MB size data',
-        data: {
-          C1: generateRandomString(MAX_LOB_SIZE),
-          C2: generateRandomString(ORIGIN_SIZE),
-          C3: Math.ceil(Math.random() * 100000)
-        },
-      },
+    const originSizeTableData = {
+      C1: generateRandomString(ORIGIN_SIZE),
+      C2: generateRandomString(ORIGIN_SIZE),
+      C3: Math.ceil(Math.random() * 100000)
+    };
+
+    const mediumSizeTableData = {
+      C1: generateRandomString(MEDIUM_SIZE),
+      C2: generateRandomString(ORIGIN_SIZE),
+      C3: Math.ceil(Math.random() * 100000)
+    };
+
+    const largeSizeTableData = {
+      C1: generateRandomString(LARGE_SIZE),
+      C2: generateRandomString(ORIGIN_SIZE),
+      C3: Math.ceil(Math.random() * 100000)
+    };
+
+    const maxSizeTableData = {
+      C1: generateRandomString(MAX_LOB_SIZE),
+      C2: generateRandomString(ORIGIN_SIZE),
+      C3: Math.ceil(Math.random() * 100000)
+    };
+
+    const testCases = [
+      { name: 'test - insert 16byte size data', data: smallSizeTableData },
+      { name: 'test - insert 16MB size data', data: originSizeTableData },
+      { name: 'test - insert 32MB size data', data: mediumSizeTableData },
+      { name: 'test - insert 64MB size data', data: largeSizeTableData },
+      { name: 'test - insert 128MB size data', data: maxSizeTableData }
     ];
 
     describe('test the increased LOB memory', function () {
-      const testSizes = [SMALL_SIZE, ORIGIN_SIZE, MEDIEUM_SIZE, LARGE_SIZE, MAX_LOB_SIZE];
+      const testSizes = [SMALL_SIZE, ORIGIN_SIZE, MEDIUM_SIZE, LARGE_SIZE, MAX_LOB_SIZE];
 
       before(async function () {
         connection = testUtil.createConnection();
@@ -92,6 +95,52 @@ if (process.env.RUN_MANUAL_TESTS_ONLY === 'true'){
         it(`test ${size} byte size data`, function (){
           testUtil.executeCmd(connection, `select randstr(${size}, 124)`, (err) => assert.ok(!err));
         });
+      });
+    });
+
+    describe('test put get max LOB size', function () {
+      before(async function () {
+        connection = testUtil.createConnection();
+        await testUtil.connectAsync(connection);
+        await testUtil.executeCmdAsync(connection, createTable);
+        await testUtil.executeCmdAsync(connection, allowLobExternalScan);
+      });
+
+      after(async () => {
+        await testUtil.executeCmdAsync(connection, removeStage);
+        await testUtil.dropTablesIgnoringErrorsAsync(connection, [tableName]);
+        await testUtil.destroyConnectionAsync(connection);
+      });
+
+      it('test - copy to stage where size > 16MB', function (done){
+        const rowData = mediumSizeTableData.C1 + ',' + mediumSizeTableData.C2 + ',' + mediumSizeTableData.C3 + '\n';
+        const tmpFile = testUtil.createTempFile(os.tmpdir(), testUtil.createRandomFileName(), rowData);
+
+        let putQuery = `PUT file://${tmpFile} ${stageName}`;
+        // Windows user contains a '~' in the path which causes an error
+        if (process.platform === 'win32') {
+          const fileName = path.basename(tmpFile);
+          putQuery = `PUT file://${process.env.USERPROFILE}\\AppData\\Local\\Temp\\${fileName} ${stageName}`;
+        }
+
+        async.series([
+          function (callback) {
+            // Upload file
+            connection.execute({
+              sqlText: putQuery,
+              complete: function (err, _, rows) {
+                if (err) {
+                  callback(err);
+                }
+                callback();
+              }
+            });
+          },
+          function (callback) {
+            // Copy into temp table
+            testUtil.executeCmd(connection, copyIntoTable, callback);
+          }
+        ], done);
       });
     });
 

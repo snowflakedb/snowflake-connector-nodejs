8ee55c3e83ac613bac39725ad223f0378c59e400
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2024-07-18 11:57:43 +0200

    SNOW-1332640 - structured type - simple types

--- test/integration/testStructuredType.js
+++ test/integration/testStructuredType.js
@@ -19,6 +19,12 @@ describe('Test Structured types', function () {
         snowflake.configure({ 'insecureConnect': true });
         GlobalConfig.setInsecureConnect(true);
         testUtil.connect(connection, callback);
+      },
+      function (callback) {
+        testUtil.executeCmd(connection, 'alter session set ENABLE_STRUCTURED_TYPES_IN_CLIENT_RESPONSE = true', callback);
+      },
+      function (callback) {
+        testUtil.executeCmd(connection, 'alter session set IGNORE_CLIENT_VESRION_IN_STRUCTURED_TYPES_RESPONSE = true', callback);
       }],
     done
     );
@@ -29,12 +35,6 @@ describe('Test Structured types', function () {
       const selectObject = 'select {\'string\':\'a\'}::OBJECT(string VARCHAR) as result';
 
       async.series([
-        function (callback) {
-          testUtil.executeCmd(connection, 'alter session set ENABLE_STRUCTURED_TYPES_IN_CLIENT_RESPONSE = true', callback);
-        },
-        function (callback) {
-          testUtil.executeCmd(connection, 'alter session set IGNORE_CLIENT_VESRION_IN_STRUCTURED_TYPES_RESPONSE = true', callback);
-        },
         function (callback) {
           testUtil.executeQueryAndVerify(
             connection,
@@ -52,42 +52,26 @@ describe('Test Structured types', function () {
         '\'timestamp_ltz\': \'2021-12-22 09:43:44\'::TIMESTAMP_LTZ' +
         '}' +
         '::OBJECT(timestamp_ltz TIMESTAMP_LTZ) As RESULT';
+      const expected = { timestamp_ltz: '2021-12-22 09:43:44.000 -0800' };
       async.series([
         function (callback) {
-          const executeOptions = {};
-          executeOptions.sqlText = selectObject;
-          executeOptions.complete = function (err, stmt) {
-            assert.ok(!err, JSON.stringify(err));
-            let rowCount = 0;
-            const stream = stmt.streamRows();
-
-            stream.on('readable', function () {
-              let row;
-              while ((row = stream.read()) !== null) {
-                const narmalizedRow = {};
-                const expected = {};
-                Object.keys(row).forEach((key) => {
-                  narmalizedRow[key] = testUtil.normalizeRowObject(row[key]);
-                });
-                expected.timestamp_ltz = '2021-12-22 09:43:44.000 -0800';
-                assert.deepStrictEqual(narmalizedRow.RESULT, expected);
-                rowCount++;
-              }
-            });
-            stream.on('error', function (err) {
-              assert.ok(!err, JSON.stringify(err));
-            });
-            stream.on('end', function () {
-              assert.strictEqual(rowCount, 1);
+          connection.execute({
+            sqlText: selectObject,
+            complete: function (err, stmt, rows) {
+              testUtil.checkError(err);
+              const row = rows[0];
+              const narmalizedRow = {};
+              Object.keys(row).forEach((key) => {
+                narmalizedRow[key] = testUtil.normalizeRowObject(row[key]);
+              });
+              assert.deepStrictEqual(narmalizedRow.RESULT, expected);
               callback();
-              done();
-            });
-          };
-          connection.execute(executeOptions);
+            }
+          });
         }
-      ]
+      ],
+      done
       );
-
     });
 
     it('test timestamp ltz fetch as string', function (done) {
@@ -95,33 +79,26 @@ describe('Test Structured types', function () {
         '\'timestamp_ltz\': \'2021-12-22 09:43:44\'::TIMESTAMP_LTZ' +
         '}' +
         '::OBJECT(timestamp_ltz TIMESTAMP_LTZ) As RESULT';
+      const expected = '{"timestamp_ltz":"2021-12-22 09:43:44.000 -0800"}';
+      
       async.series([
         function (callback) {
-          const executeOptions = {};
-          executeOptions.sqlText = selectObject;
-          executeOptions.fetchAsString = [snowflake.OBJECT];
-          executeOptions.complete = function (err, stmt) {
-            assert.ok(!err, JSON.stringify(err));
-            let rowCount = 0;
-            const stream = stmt.streamRows();
-            stream.on('readable', function () {
-              let row;
-              while ((row = stream.read()) !== null) {
-                const expected = '{"timestamp_ltz":"2021-12-22 09:43:44.000 -0800"}';
-                assert.deepStrictEqual(row.RESULT, expected);
-                rowCount++;
-                rowCount++;
-              }
-            });
-            stream.on('error', function (err) {
-              assert.ok(!err, JSON.stringify(err));
-            });
-            stream.on('end', function () {
+          connection.execute({
+            sqlText: selectObject,
+            fetchAsString: [snowflake.OBJECT],
+            complete: function (err, stmt, rows) {
+              testUtil.checkError(err);
+              const row = rows[0];
+              const narmalizedRow = {};
+              Object.keys(row).forEach((key) => {
+                narmalizedRow[key] = testUtil.normalizeRowObject(row[key]);
+              });
+              assert.deepStrictEqual(row.RESULT, expected);
               callback();
-            });
-          };
-          connection.execute(executeOptions);
-        }],
+            }
+          });
+        },
+      ],
       done
       );
     });
@@ -131,41 +108,26 @@ describe('Test Structured types', function () {
         '\'timestamp_ntz\': \'2021-12-22 09:43:44\'::TIMESTAMP_NTZ' +
         '}' +
         '::OBJECT(timestamp_ntz TIMESTAMP_NTZ) AS RESULT';
+      const expected = { timestamp_ntz: '2021-12-22 09:43:44.000' };
+      
       async.series([
         function (callback) {
-          const executeOptions = {};
-          executeOptions.sqlText = selectObject;
-          executeOptions.complete = function (err, stmt) {
-            assert.ok(!err, JSON.stringify(err));
-            let rowCount = 0;
-            const stream = stmt.streamRows();
-
-            stream.on('readable', function () {
-              let row;
-              while ((row = stream.read()) !== null) {
-                const narmalizedRow = {};
-                const expected = {};
-                Object.keys(row).forEach((key) => {
-                  narmalizedRow[key] = testUtil.normalizeRowObject(row[key]);
-                });
-                expected.timestamp_ntz = '2021-12-22 09:43:44.000';
-                console.log(narmalizedRow);
-                assert.deepStrictEqual(narmalizedRow.RESULT, expected);
-                rowCount++;
-              }
-            });
-            stream.on('error', function (err) {
-              assert.ok(!err, JSON.stringify(err));
-            });
-            stream.on('end', function () {
-              assert.strictEqual(rowCount, 1);
+          connection.execute({
+            sqlText: selectObject,
+            complete: function (err, stmt, rows) {
+              testUtil.checkError(err);
+              const row = rows[0];
+              const narmalizedRow = {};
+              Object.keys(row).forEach((key) => {
+                narmalizedRow[key] = testUtil.normalizeRowObject(row[key]);
+              });
+              assert.deepStrictEqual(narmalizedRow.RESULT, expected);
               callback();
-              done();
-            });
-          };
-          connection.execute(executeOptions);
-        }
-      ]
+            }
+          });
+        },
+      ],
+      done
       );
 
     });
@@ -175,43 +137,29 @@ describe('Test Structured types', function () {
         '\'timestamp_tz\': \'2021-12-24 09:45:45 -0800\'::TIMESTAMP_TZ' +
         '}' +
         '::OBJECT(timestamp_tz TIMESTAMP_TZ) AS RESULT';
+      const expected = { timestamp_tz: '2021-12-24 09:45:45.000 -0800' };
+
       async.series([
         function (callback) {
           testUtil.executeCmd(connection, sharedStatement.setTimezoneAndTimestamps, callback);
         },
         function (callback) {
-          const executeOptions = {};
-          executeOptions.sqlText = selectObject;
-          executeOptions.complete = function (err, stmt) {
-            assert.ok(!err, JSON.stringify(err));
-            let rowCount = 0;
-            const stream = stmt.streamRows();
-
-            stream.on('readable', function () {
-              let row;
-              while ((row = stream.read()) !== null) {
-                const narmalizedRow = {};
-                const expected = {};
-                Object.keys(row).forEach((key) => {
-                  narmalizedRow[key] = testUtil.normalizeRowObject(row[key]);
-                });
-                expected.timestamp_tz = '2021-12-24 09:45:45.000 -0800';
-                assert.deepStrictEqual(narmalizedRow.RESULT, expected);
-                rowCount++;
-              }
-            });
-            stream.on('error', function (err) {
-              assert.ok(!err, JSON.stringify(err));
-            });
-            stream.on('end', function () {
-              assert.strictEqual(rowCount, 1);
+          connection.execute({
+            sqlText: selectObject,
+            complete: function (err, stmt, rows) {
+              testUtil.checkError(err);
+              const row = rows[0];
+              const narmalizedRow = {};
+              Object.keys(row).forEach((key) => {
+                narmalizedRow[key] = testUtil.normalizeRowObject(row[key]);
+              });
+              assert.deepStrictEqual(narmalizedRow.RESULT, expected);
               callback();
-              done();
-            });
-          };
-          connection.execute(executeOptions);
-        }
-      ]
+            }
+          });
+        },
+      ],
+      done
       );
 
     });
@@ -221,14 +169,24 @@ describe('Test Structured types', function () {
         '\'binary\': TO_BINARY(\'616263\', \'HEX\')' +
         '}' +
         '::OBJECT(binary BINARY) As RESULT';
+      
+      const expected = { RESULT: { 'binary': [97, 98, 99] } };
+      
       async.series([
         function (callback) {
-          testUtil.executeQueryAndVerify(
-            connection,
-            selectObject,
-            [{ RESULT: { 'binary': [97, 98, 99] } }],
-            callback
-          );
+          connection.execute({
+            sqlText: selectObject,
+            complete: function (err, stmt, rows) {
+              testUtil.checkError(err);
+              const row = rows[0];
+              const narmalizedRow = {};
+              Object.keys(row).forEach((key) => {
+                narmalizedRow[key] = testUtil.normalizeRowObject(row[key]);
+              });
+              assert.deepStrictEqual(narmalizedRow, expected);
+              callback();
+            }
+          });
         }],
       done
       );
@@ -267,55 +225,46 @@ describe('Test Structured types', function () {
         'binary BINARY' +
         ') AS RESULT';
 
-      const expected = { RESULT: {} };
-      expected.RESULT = { string: 'a',
-        b: 1,
-        s: 2,
-        i: 3,
-        l: 4,
-        f: 1.1,
-        d: 2.2,
-        bd: 3.3,
-        bool: true,
-        timestamp_ltz: '2021-12-22 09:43:44.000 -0800',
-        timestamp_ntz: '2021-12-23 09:44:44.000',
-        timestamp_tz: '2021-12-24 09:45:45.000 -0800',
-        date: '2023-12-23',
-        time: '03:34:56',
-        binary: [97, 98, 99] };
+      const expected = {
+        RESULT: {
+          string: 'a',
+          b: 1,
+          s: 2,
+          i: 3,
+          l: 4,
+          f: 1.1,
+          d: 2.2,
+          bd: 3.3,
+          bool: true,
+          timestamp_ltz: '2021-12-22 09:43:44.000 -0800',
+          timestamp_ntz: '2021-12-23 09:44:44.000',
+          timestamp_tz: '2021-12-24 09:45:45.000 -0800',
+          date: '2023-12-23',
+          time: '03:34:56',
+          binary: [97, 98, 99]
+        }
+      };
 
       async.series([
         function (callback) {
           testUtil.executeCmd(connection, sharedStatement.setTimezoneAndTimestamps, callback);
         },
         function (callback) {
-          const executeOptions = {};
-          executeOptions.sqlText = selectObject;
-          executeOptions.complete = function (err, stmt) {
-            assert.ok(!err, JSON.stringify(err));
-            let rowCount = 0;
-            const stream = stmt.streamRows();
-            stream.on('readable', function () {
-              let row;
-              while ((row = stream.read()) !== null) {
-                const narmalizedRow = {};
-                Object.keys(row).forEach((key) => {
-                  narmalizedRow[key] = testUtil.normalizeRowObject(row[key]);
-                });
-                assert.deepStrictEqual(narmalizedRow, expected);
-                rowCount++;
-              }
-            });
-            stream.on('error', function (err) {
-              assert.ok(!err, JSON.stringify(err));
-            });
-            stream.on('end', function () {
-              assert.strictEqual(rowCount, 1);
+          connection.execute({
+            sqlText: selectObject,
+            complete: function (err, stmt, rows) {
+              testUtil.checkError(err);
+              const row = rows[0];
+              const narmalizedRow = {};
+              Object.keys(row).forEach((key) => {
+                narmalizedRow[key] = testUtil.normalizeRowObject(row[key]);
+              });
+              assert.deepStrictEqual(narmalizedRow, expected);
               callback();
-            });
-          };
-          connection.execute(executeOptions);
-        }],
+            }
+          });
+        },
+      ],
       done
       );
     });
@@ -354,41 +303,44 @@ describe('Test Structured types', function () {
         ') AS RESULT';
 
       const expected = {
-        "RESULT": "{\"string\":\"a\",\"b\":1,\"s\":2,\"i\":3,\"l\":4,\"f\":1.1,\"d\":2.2,\"bd\":3.3,\"bool\":true,\"timestamp_ltz\":\"2021-12-22 09:43:44.000 -0800\",\"timestamp_ntz\":\"2021-12-23 09:44:44.000\",\"timestamp_tz\":\"2021-12-24 09:45:45.000 -0800\",\"date\":\"2023-12-23\",\"time\":\"03:34:56\",\"binary\":[97,98,99]}"
-      }
+        'RESULT': '{"string":"a","b":1,"s":2,"i":3,"l":4,"f":1.1,"d":2.2,"bd":3.3,"bool":true,"timestamp_ltz":"2021-12-22 09:43:44.000 -0800","timestamp_ntz":"2021-12-23 09:44:44.000","timestamp_tz":"2021-12-24 09:45:45.000 -0800","date":"2023-12-23","time":"03:34:56","binary":[97,98,99]}'
+      };
 
       async.series([
         function (callback) {
           testUtil.executeCmd(connection, sharedStatement.setTimezoneAndTimestamps, callback);
         },
         function (callback) {
-          const executeOptions = {};
-          executeOptions.sqlText = selectObject;
-          executeOptions.fetchAsString = [snowflake.OBJECT];
-          executeOptions.complete = function (err, stmt) {
-            assert.ok(!err, JSON.stringify(err));
-            let rowCount = 0;
-            const stream = stmt.streamRows();
-            stream.on('readable', function () {
-              let row;
-              while ((row = stream.read()) !== null) {
-                assert.deepStrictEqual(row, expected);
-                rowCount++;
-              }
-            });
-            stream.on('error', function (err) {
-              assert.ok(!err, JSON.stringify(err));
-            });
-            stream.on('end', function () {
-              assert.strictEqual(rowCount, 1);
+          connection.execute({
+            sqlText: selectObject,
+            fetchAsString: [snowflake.OBJECT],
+            complete: function (err, stmt, rows) {
+              testUtil.checkError(err);
+              const row = rows[0];
+              assert.deepStrictEqual(row, expected);
               callback();
-            });
-          };
-          connection.execute(executeOptions);
-        }],
+            }
+          });
+        },
+      ],
       done
       );
     });
-  });
 
+    // it('test nested object', function (done) {
+    //   const selectObject = 'select {\'inside\': {\'string\':\'a\'}}::OBJECT(inside OBJECT(string VARCHAR)) as result';
+    //
+    //   async.series([
+    //     function (callback) {
+    //       testUtil.executeQueryAndVerify(
+    //         connection,
+    //         selectObject,
+    //         [{ RESULT: { 'string': 'a' } }],
+    //         callback
+    //       );
+    //     }],
+    //   done
+    //   );
+    // });
+  });
 });

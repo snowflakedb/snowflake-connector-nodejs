431d44847f665e1ba1ff2693e4c59dd3c7a8b579
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2024-08-02 08:26:28 +0200

    SNOW-1332640 - structured type - object

--- test/integration/testStructuredType.js
+++ test/integration/testStructuredType.js
@@ -9,7 +9,7 @@ const testUtil = require('./testUtil');
 const sharedStatement = require('./sharedStatements');
 const assert = require('assert');
 
-describe('Test Structured types', function () {
+describe.only('Test Structured types', function () {
   let connection;
 
   before(function (done) {
@@ -25,6 +25,9 @@ describe('Test Structured types', function () {
       },
       function (callback) {
         testUtil.executeCmd(connection, 'alter session set IGNORE_CLIENT_VESRION_IN_STRUCTURED_TYPES_RESPONSE = true', callback);
+      },
+      function (callback) {
+        testUtil.executeCmd(connection, sharedStatement.setTimestampOutputFormat, callback);
       }],
     done
     );
@@ -49,10 +52,10 @@ describe('Test Structured types', function () {
 
     it('test timestamp ltz', function (done) {
       const selectObject = 'select {' +
-        '\'timestamp_ltz\': \'2021-12-22 09:43:44\'::TIMESTAMP_LTZ' +
+        '\'timestampltz\': \'2021-12-22 09:43:44\'::TIMESTAMP_LTZ' +
         '}' +
-        '::OBJECT(timestamp_ltz TIMESTAMP_LTZ) As RESULT';
-      const expected = { timestamp_ltz: '2021-12-22 09:43:44.000 -0800' };
+        '::OBJECT(timestampltz TIMESTAMP_LTZ) As RESULT';
+      const expected = { timestampltz: '2021-12-22 09:43:44.000 -0800' };
       async.series([
         function (callback) {
           connection.execute({
@@ -76,10 +79,10 @@ describe('Test Structured types', function () {
 
     it('test timestamp ltz fetch as string', function (done) {
       const selectObject = 'select {' +
-        '\'timestamp_ltz\': \'2021-12-22 09:43:44\'::TIMESTAMP_LTZ' +
+        '\'timestampltz\': \'2021-12-22 09:43:44\'::TIMESTAMP_LTZ' +
         '}' +
-        '::OBJECT(timestamp_ltz TIMESTAMP_LTZ) As RESULT';
-      const expected = '{"timestamp_ltz":"2021-12-22 09:43:44.000 -0800"}';
+        '::OBJECT(timestampltz TIMESTAMP_LTZ) As RESULT';
+      const expected = '{"timestampltz":"2021-12-22 09:43:44.000 -0800"}';
 
       async.series([
         function (callback) {
@@ -105,10 +108,10 @@ describe('Test Structured types', function () {
 
     it('test timestamp ntz', function (done) {
       const selectObject = 'select {' +
-        '\'timestamp_ntz\': \'2021-12-22 09:43:44\'::TIMESTAMP_NTZ' +
+        '\'timestampntz\': \'2021-12-22 09:43:44\'::TIMESTAMP_NTZ' +
         '}' +
-        '::OBJECT(timestamp_ntz TIMESTAMP_NTZ) AS RESULT';
-      const expected = { timestamp_ntz: '2021-12-22 09:43:44.000' };
+        '::OBJECT(timestampntz TIMESTAMP_NTZ) AS RESULT';
+      const expected = { timestampntz: '2021-12-22 09:43:44.000' };
 
       async.series([
         function (callback) {
@@ -134,10 +137,10 @@ describe('Test Structured types', function () {
 
     it('test timestamp tz', function (done) {
       const selectObject = 'select {' +
-        '\'timestamp_tz\': \'2021-12-24 09:45:45 -0800\'::TIMESTAMP_TZ' +
+        '\'timestamptz\': \'2021-12-24 09:45:45 -0800\'::TIMESTAMP_TZ' +
         '}' +
-        '::OBJECT(timestamp_tz TIMESTAMP_TZ) AS RESULT';
-      const expected = { timestamp_tz: '2021-12-24 09:45:45.000 -0800' };
+        '::OBJECT(timestamptz TIMESTAMP_TZ) AS RESULT';
+      const expected = { timestamptz: '2021-12-24 09:45:45.000 -0800' };
 
       async.series([
         function (callback) {
@@ -266,9 +269,9 @@ describe('Test Structured types', function () {
         ' \'d\': 2.2,' +
         ' \'bd\': 3.3, ' +
         '\'bool\': true, ' +
-        '\'timestamp_ltz\': \'2021-12-22 09:43:44\'::TIMESTAMP_LTZ,' +
-        ' \'timestamp_ntz\': \'2021-12-23 09:44:44\'::TIMESTAMP_NTZ, ' +
-        '\'timestamp_tz\': \'2021-12-24 09:45:45 -0800\'::TIMESTAMP_TZ,' +
+        '\'timestampltz\': \'2021-12-22 09:43:44\'::TIMESTAMP_LTZ,' +
+        ' \'timestampntz\': \'2021-12-23 09:44:44\'::TIMESTAMP_NTZ, ' +
+        '\'timestamptz\': \'2021-12-24 09:45:45 -0800\'::TIMESTAMP_TZ,' +
         ' \'date\': \'2023-12-24\'::DATE, ' +
         '\'time\': \'12:34:56\'::TIME, ' +
         '\'binary\': TO_BINARY(\'616263\', \'HEX\') ' +
@@ -282,9 +285,9 @@ describe('Test Structured types', function () {
         'd DOUBLE, ' +
         'bd DOUBLE, ' +
         'bool BOOLEAN,' +
-        'timestamp_ltz TIMESTAMP_LTZ,' +
-        'timestamp_ntz TIMESTAMP_NTZ, ' +
-        'timestamp_tz TIMESTAMP_TZ, ' +
+        'timestampltz TIMESTAMP_LTZ,' +
+        'timestampntz TIMESTAMP_NTZ, ' +
+        'timestamptz TIMESTAMP_TZ, ' +
         'date DATE, time TIME, ' +
         'binary BINARY' +
         ') AS RESULT';
@@ -300,9 +303,9 @@ describe('Test Structured types', function () {
           d: 2.2,
           bd: 3.3,
           bool: true,
-          timestamp_ltz: '2021-12-22 09:43:44.000 -0800',
-          timestamp_ntz: '2021-12-23 09:44:44.000',
-          timestamp_tz: '2021-12-24 09:45:45.000 -0800',
+          timestampltz: '2021-12-22 09:43:44.000 -0800',
+          timestampntz: '2021-12-23 09:44:44.000',
+          timestamptz: '2021-12-24 09:45:45.000 -0800',
           date: '2023-12-23',
           time: '12:34:56',
           binary: [97, 98, 99]
@@ -343,9 +346,9 @@ describe('Test Structured types', function () {
         ' \'d\': 2.2,' +
         ' \'bd\': 3.3, ' +
         '\'bool\': true, ' +
-        '\'timestamp_ltz\': \'2021-12-22 09:43:44\'::TIMESTAMP_LTZ,' +
-        ' \'timestamp_ntz\': \'2021-12-23 09:44:44\'::TIMESTAMP_NTZ, ' +
-        '\'timestamp_tz\': \'2021-12-24 09:45:45 -0800\'::TIMESTAMP_TZ,' +
+        '\'timestampltz\': \'2021-12-22 09:43:44\'::TIMESTAMP_LTZ,' +
+        ' \'timestampntz\': \'2021-12-23 09:44:44\'::TIMESTAMP_NTZ, ' +
+        '\'timestamptz\': \'2021-12-24 09:45:45 -0800\'::TIMESTAMP_TZ,' +
         ' \'date\': \'2023-12-24\'::DATE, ' +
         '\'time\': \'12:34:56\'::TIME, ' +
         '\'binary\': TO_BINARY(\'616263\', \'HEX\') ' +
@@ -359,15 +362,15 @@ describe('Test Structured types', function () {
         'd DOUBLE, ' +
         'bd DOUBLE, ' +
         'bool BOOLEAN,' +
-        'timestamp_ltz TIMESTAMP_LTZ,' +
-        'timestamp_ntz TIMESTAMP_NTZ, ' +
-        'timestamp_tz TIMESTAMP_TZ, ' +
+        'timestampltz TIMESTAMP_LTZ,' +
+        'timestampntz TIMESTAMP_NTZ, ' +
+        'timestamptz TIMESTAMP_TZ, ' +
         'date DATE, time TIME, ' +
         'binary BINARY' +
         ') AS RESULT';
 
       const expected = {
-        'RESULT': '{"string":"a","b":1,"s":2,"i":3,"l":4,"f":1.1,"d":2.2,"bd":3.3,"bool":true,"timestamp_ltz":"2021-12-22 09:43:44.000 -0800","timestamp_ntz":"2021-12-23 09:44:44.000","timestamp_tz":"2021-12-24 09:45:45.000 -0800","date":"2023-12-23","time":"12:34:56","binary":[97,98,99]}'
+        'RESULT': '{"string":"a","b":1,"s":2,"i":3,"l":4,"f":1.1,"d":2.2,"bd":3.3,"bool":true,"timestampltz":"2021-12-22 09:43:44.000 -0800","timestampntz":"2021-12-23 09:44:44.000","timestamptz":"2021-12-24 09:45:45.000 -0800","date":"2023-12-23","time":"12:34:56","binary":[97,98,99]}'
       };
 
       async.series([

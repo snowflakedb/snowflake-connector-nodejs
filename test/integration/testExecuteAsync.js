f3a759a9db8fdae17f74dbd149898c219c4e29e6
John Yun <jonghun.yun@insightsoftware.com>
2024-08-13 00:34:50 +0900

    added new error and modified the testing case

--- test/integration/testExecuteAsync.js
+++ test/integration/testExecuteAsync.js
@@ -230,8 +230,7 @@ describe('ExecuteAsync test', function () {
     assert.strictEqual(QueryStatus[status], QueryStatus.NO_QUERY_DATA);
   });
 
-  // The test retries until it reaches the max retry count and sometimes it fails due to timeout
-  it.skip('testGetResultsOfUnknownQueryId', async function () {
+  it('testGetResultsOfUnknownQueryId', async function () {
     const unknownQueryId = '12345678-1234-4123-A123-123456789012';
 
     // Get the query results using an unknown query id

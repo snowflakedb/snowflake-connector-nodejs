48234301000136e670c6abf272a7ec94cef546a8
Adam Kolodziejczyk <adam.kolodziejczyk@snowflake.com>
2024-10-08 16:00:05 +0200

    replace centos test images with node 14 with chainguard images with node 18

--- ci/_init.sh
+++ ci/_init.sh
@@ -15,20 +15,25 @@ fi
 export DRIVER_NAME=nodejs
 
 # Build images
-BUILD_IMAGE_VERSION=2
+BUILD_IMAGE_VERSION=1
 
 # Test Images
-TEST_IMAGE_VERSION=2
+TEST_IMAGE_VERSION=1
 
 declare -A BUILD_IMAGE_NAMES=(
-    [$DRIVER_NAME-centos7-node14]=$DOCKER_REGISTRY_NAME/client-$DRIVER_NAME-centos7-node14-build:$BUILD_IMAGE_VERSION
+    [$DRIVER_NAME-chainguard-node18]=$DOCKER_REGISTRY_NAME/client-$DRIVER_NAME-chainguard-node18-build:$BUILD_IMAGE_VERSION
 )
 export BUILD_IMAGE_NAMES
 
 declare -A TEST_IMAGE_NAMES=(
-    [$DRIVER_NAME-centos7-fips]=$DOCKER_REGISTRY_NAME/client-$DRIVER_NAME-centos7-fips-test:$BUILD_IMAGE_VERSION
-    [$DRIVER_NAME-centos7-node14]=$DOCKER_REGISTRY_NAME/client-$DRIVER_NAME-centos7-node14-test:$BUILD_IMAGE_VERSION
-
+    [$DRIVER_NAME-chainguard-node18]=$DOCKER_REGISTRY_NAME/client-$DRIVER_NAME-chainguard-node18-test:$TEST_IMAGE_VERSION
+    [$DRIVER_NAME-chainguard-node18-fips]=$DOCKER_REGISTRY_NAME/client-$DRIVER_NAME-chainguard-node18-fips-test:$TEST_IMAGE_VERSION
 )
 export TEST_IMAGE_NAMES
 
+BASE_IMAGE_PATH=artifactory.int.snowflakecomputing.com/development-chainguard-virtual/snowflake.com
+declare -A BASE_IMAGES=(
+    [$DRIVER_NAME-chainguard-node18]=$BASE_IMAGE_PATH/"node:18-dev"
+    [$DRIVER_NAME-chainguard-node18-fips]=$BASE_IMAGE_PATH/"node-fips:18-dev"
+)
+export BASE_IMAGES

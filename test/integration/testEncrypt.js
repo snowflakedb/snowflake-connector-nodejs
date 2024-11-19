b222951e949a0ac54767a6838abfec2ee76d87e1
Dawid Heyman <dawid.heyman@snowflake.com>
2024-11-19 14:33:11 +0100

    Add tests

---
+++ test/integration/testEncrypt.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const SnowflakeEncryptionUtil = require('../../lib/file_transfer_agent/encrypt_util').EncryptUtil;
+
+let encryptUtil;
+
+describe('Test Encryption/Decryption', function () {
+  before(function () {
+    encryptUtil = new SnowflakeEncryptionUtil();
+  });
+
+  it('GCM - Encrypt and decrypt raw data', function () {
+    const data = 'abc';
+    const iv = Buffer.from('ab1234567890');
+    const key = Buffer.from('1234567890abcdef');
+
+    const encryptedData = encryptUtil.encryptGCM(data, key, iv, null);
+    assert.strictEqual(encryptedData.toString('base64'), 'iG+lT4o27hkzj3kblYRzQikLVQ==');
+
+    const decryptedData = encryptUtil.decryptGCM(encryptedData, key, iv, null);
+    assert.strictEqual(decryptedData.toString('utf-8'), data);
+  });
+
+  it('GCM - Encrypt and decrypt file', async function () {
+    await encryptAndDecryptFile('gcm', async function (encryptionMaterial, inputFilePath) {
+      const output = await encryptUtil.encryptFileGCM(encryptionMaterial, inputFilePath, os.tmpdir());
+      return await encryptUtil.decryptFileGCM(output.encryptionMetadata, encryptionMaterial, output.dataFile, os.tmpdir());
+    });
+  });
+
+  it('CBC - Encrypt and decrypt file', async function () {
+    await encryptAndDecryptFile('cbc', async function (encryptionMaterial, inputFilePath) {
+      const output = await encryptUtil.encryptFileCBC(encryptionMaterial, inputFilePath, os.tmpdir());
+      return await encryptUtil.decryptFileCBC(output.encryptionMetadata, encryptionMaterial, output.dataFile, os.tmpdir());
+    });
+  });
+  
+  async function encryptAndDecryptFile(encryptionTypeName, encryptAndDecrypt) {
+    const data = 'abc';
+    const inputFilePath = path.join(os.tmpdir(), `${encryptionTypeName}_file_encryption_test`);
+    fs.writeFileSync(inputFilePath, data);
+
+    const encryptionMaterial = {
+      'queryStageMasterKey': 'YWJjZGVmMTIzNDU2Nzg5MA==',
+      'queryId': 'unused',
+      'smkId': '123'
+    };
+    const decryptedFilePath = await encryptAndDecrypt(encryptionMaterial, inputFilePath, os.tmpdir());
+    const decryptedContent = fs.readFileSync(decryptedFilePath);
+    assert.strictEqual(decryptedContent.toString('utf-8'), data);
+  }
+});

dbe5de6ae4282f76baa9fc08be99c4fd19c9fcf5
Filip Pawłowski <filip.pawlowski@snowflake.com>
2024-12-18 15:19:00 +0100

    SNOW-1814745-Fix-aborting-requests-in-HttpClient: Made tests functionalities more reusable. Fixed raising error in tests in callbacks.

--- test/integration/testHttpClient.js
+++ test/integration/testHttpClient.js
@@ -10,7 +10,7 @@ describe('HttpClient Tests', () => {
 
   const connectionOptions = {
     ...(connOptions.valid),
-    timeout: 100000
+    timeout: 2000
   };
 
   const httpConnectionOptions = new ConnectionConfig(connectionOptions, false, false, {
@@ -23,42 +23,49 @@ describe('HttpClient Tests', () => {
   });
 
   describe('Aborting requests', () => {
+    const urlReturningResponseAfterHanging = hangWebServerUrl + '/hang';
+
     it('should allow aborting any request immediately', async () => {
+      let errorFromCallback;
+
       const requestObject = httpClientInstance.request({
-        url: hangWebServerUrl + '/hang',
+        url: urlReturningResponseAfterHanging,
         method: 'GET',
         callback: (err) => {
           // We expect an error due to aborting the request.
           if (err) {
             testUtil.isRequestCancelledError(err);
-            assert.equal(err.message, 'canceled');
-            assert.equal(err.name, 'CanceledError');
-            assert.equal(err.code, 'ERR_CANCELED');
           } else {
-            throw Error('Expected an error from aborted request, but got success.');
+            errorFromCallback = Error('Expected an error from aborted request, but got success.');
           }
         }
       });
 
       // Abort the request immediately
       requestObject.abort();
+
+      //Due to usage of 'nextTick' in the httpClient requestPromise may be undefined for some time, only to be set when scheduled sending took place.
+      await testUtil.waitForCondition(() => requestObject.requestPromise);
       await requestObject.requestPromise;
+
+      assert.ok(!errorFromCallback, `Did not receive a normalized response. Error: ${errorFromCallback}`);
     });
 
     it('should allow aborting long-running request after some time', async () => {
-      const TIME_IN_MS_TO_WAIT_BEFORE_ABORT = 2000;
+      let errorFromCallback;
+      const TIME_IN_MS_TO_WAIT_BEFORE_ABORT = 1500;
       assert.ok(TIME_IN_MS_TO_WAIT_BEFORE_ABORT < connectionOptions.timeout, 'Test was not set up correctly. ' +
           'To test correctly the aborting functionality it should be triggered before timeout of the request itself');
 
       const requestObject = httpClientInstance.request({
-        url: hangWebServerUrl + '/hang',
+        url: urlReturningResponseAfterHanging,
         method: 'GET',
         callback: (err) => {
           // We expect an error due to aborting the request.
           if (err) {
             testUtil.isRequestCancelledError(err);
           } else {
-            throw Error('Expected an error from aborted request, but got success.');
+            errorFromCallback = Error('Expected an error from aborted request, but got success.');
           }
         }
       });
@@ -66,16 +73,21 @@ describe('HttpClient Tests', () => {
       // Abort the request after some time
       await testUtil.sleepAsync(TIME_IN_MS_TO_WAIT_BEFORE_ABORT);
       requestObject.abort();
+
+      //Due to usage of 'nextTick' in the httpClient requestPromise may be undefined for some time, only to be set when scheduled sending took place.
+      await testUtil.waitForCondition(() => requestObject.requestPromise);
       await requestObject.requestPromise;
+
+      assert.ok(!errorFromCallback, `Did not receive a normalized response. Error: ${errorFromCallback}`);
     });
   });
 
   describe('Normalizing Response', () => {
-    it('should return a normalized response with statusCode and body for requestAsync', async () => {
-      const testUrl = hangWebServerUrl + '/json';
+    const urlReturningJsonBody = hangWebServerUrl + '/json';
 
+    it('should return a normalized response with statusCode and body for requestAsync', async () => {
       const response = await httpClientInstance.requestAsync({
-        url: testUrl,
+        url: urlReturningJsonBody,
         method: 'GET'
       });
 
@@ -85,26 +97,23 @@ describe('HttpClient Tests', () => {
     });
 
     it('should return a normalized response with statusCode and body for synchronous request', async () => {
-      const testUrl = hangWebServerUrl + '/json';
       let errorRaisedInCallback;
 
       const requestObject = httpClientInstance.request({
-        url: testUrl,
+        url: urlReturningJsonBody,
         method: 'GET',
         callback: (err, response) => {
           try {
             assert.ok(response, 'Response should be defined');
             assert.ok(response.statusCode, 'Normalized response should have statusCode');
             assert.ok(response.body, 'Normalized response should have body');
-          } catch (err) {
-            errorRaisedInCallback = err;
+          } catch (assertionError) {
+            errorRaisedInCallback = assertionError;
           }
         }
       });
-      //Due to usage of 'nextTick' in the httpClient requestPromise may be undefined for some time, only to be set in when scheduled sending took place.
-      while (!requestObject.requestPromise) {
-        await testUtil.sleepAsync(1000);
-      }
+      //Due to usage of 'nextTick' in the httpClient requestPromise may be undefined for some time, only to be set when scheduled sending took place.
+      await testUtil.waitForCondition(() => requestObject.requestPromise);
       await requestObject.requestPromise;
 
       assert.ok(!errorRaisedInCallback, `Did not receive a normalized response. Error: ${errorRaisedInCallback}`);
--- test/integration/testUtil.js
+++ test/integration/testUtil.js
@@ -349,8 +349,22 @@ module.exports.createRandomFileName = function ( option = { prefix: '', postfix:
   return fileName;
 };
 
-module.exports.sleepAsync = function (ms) {
+const sleepAsync = function (ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+module.exports.sleepAsync = sleepAsync;
+
+module.exports.waitForCondition = async function (conditionCallable, { maxWaitTimeInMs = 20000, waitTimeBetweenChecksInMs = 1000 } = {}) {
+  let waitedTimeInMs = 0;
+  while (!conditionCallable()) {
+    await sleepAsync(waitTimeBetweenChecksInMs);
+    waitedTimeInMs += waitTimeBetweenChecksInMs;
+
+    if (waitedTimeInMs > maxWaitTimeInMs) {
+      throw Error(`Condition was not met after max wait time = ${maxWaitTimeInMs}`);
+    }
+  }
 };
 
 module.exports.assertConnectionActive = function (connection) {
@@ -376,6 +390,6 @@ module.exports.isGuidInRequestOptions = function (requestOptions) {
 
 module.exports.isRequestCancelledError = function (error) {
   assert.equal(error.message, 'canceled', `Expected error message "canceled", but received ${error.message}`);
-  assert.equal(error.name, 'CanceledError', `Expected error name "canceled", but received ${error.name}`);
-  assert.equal(error.code, 'ERR_CANCELED', `Expected error code "canceled", but received ${error.code}`);
+  assert.equal(error.name, 'CanceledError', `Expected error name "CanceledError", but received ${error.name}`);
+  assert.equal(error.code, 'ERR_CANCELED', `Expected error code "ERR_CANCELED", but received ${error.code}`);
 };

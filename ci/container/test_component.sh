840b23d0057f92d7d02bcfdecef393bffb5754e2
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2025-02-28 16:38:50 +0100

    SNOW-1944643 - network error retries

--- ci/container/test_component.sh
+++ ci/container/test_component.sh
@@ -25,7 +25,7 @@ else
     export PATH=$WORKSPACE/node_modules/.bin:$PATH
 fi
 cp $SOURCE_ROOT/ci/container/package.json .
-npm install
+npm install --save-dev
 
 PACKAGE_NAME=$(cd $WORKSPACE && ls snowflake-sdk*.tgz)
 npm install $WORKSPACE/${PACKAGE_NAME}

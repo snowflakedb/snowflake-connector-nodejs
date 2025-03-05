73f3fa18e2b0f68165b88c80f450999e36298cf2
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2025-03-05 10:41:49 +0100

    SNOW-1926267: Fix promise rejecting for file upload errors

--- test/wiremockRunner.js
+++ test/wiremockRunner.js
@@ -11,7 +11,6 @@ async function runWireMockAsync(port) {
       exec(`npx wiremock --enable-browser-proxying --proxy-pass-through  false --port ${port} `);
       const wireMock = new WireMockRestClient(`http://localhost:${port}`, { logLevel: 'debug' });
       const readyWireMock =  waitForWiremockStarted(wireMock);
-      console.log("########## WIREMOCK STARTED ##############")
       resolve(readyWireMock);
     } catch (err) {
       reject(err);

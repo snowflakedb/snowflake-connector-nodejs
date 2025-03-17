fb3b09a76c9f5700a0260e3a652c0d3cca7b35e8
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2025-03-17 20:12:38 +0100

    SNOW-1825478-PAT-support

--- test/wiremockRunner.js
+++ test/wiremockRunner.js
@@ -19,7 +19,7 @@ async function runWireMockAsync(port) {
 
   const timeout = new Promise((resolve, reject) =>
     timeoutHandle = setTimeout(
-      () => reject('Wiremock unavailable  after 30s.'),
+      () => reject('Wiremock unavailable after 30s.'),
       30000));
   return Promise.race([waitingWireMockPromise, timeout])
     .then(result => {
@@ -29,6 +29,7 @@ async function runWireMockAsync(port) {
 }
 
 async function waitForWiremockStarted(wireMock) {
+  let counter = 30;
   return fetch(wireMock.baseUri)
     .then(async (resp) => {
       if (resp.ok) {
@@ -36,13 +37,21 @@ async function waitForWiremockStarted(wireMock) {
       } else {
         await new Promise(resolve => setTimeout(resolve, 1000));
         Logger.getInstance().info(`Retry connection to WireMock after wrong response status: ${resp.status}`);
-        return await waitForWiremockStarted(wireMock);
+        if (++counter < 30) { //stop after 30s
+          return await waitForWiremockStarted(wireMock);
+        } else {
+          Promise.reject('Wiremock: Waiting time has expired');
+        }
       }
     })
     .catch(async (err) => {
       await new Promise(resolve => setTimeout(resolve, 1000));
       Logger.getInstance().info(`Retry connection to WireMock after error: ${err}`);
-      return await waitForWiremockStarted(wireMock);
+      if (++counter < 30) { //stop after 30s
+        return await waitForWiremockStarted(wireMock);
+      } else {
+        Promise.reject('Wiremock: Waiting time has expired');
+      }
     });
 }
 

0445787b5a9945d22b66eee60e409d636bccb459
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2025-03-19 11:34:28 +0100

    Snow 1825478  Implement programmatic access token (PAT) (#1022)

---
+++ test/wiremockRunner.js
@@ -0,0 +1,67 @@
+const WireMockRestClient =  require('wiremock-rest-client').WireMockRestClient;
+const { exec } = require('child_process');
+const Logger = require('../lib/logger');
+const fs = require('fs');
+
+
+async function runWireMockAsync(port) {
+  let timeoutHandle;
+  const counter = 0;
+  const waitingWireMockPromise =  new Promise( (resolve, reject) => {
+    try {
+      exec(`npx wiremock --enable-browser-proxying --proxy-pass-through  false --port ${port} `);
+      const wireMock = new WireMockRestClient(`http://localhost:${port}`, { logLevel: 'debug' });
+      const readyWireMock =  waitForWiremockStarted(wireMock, counter);
+      resolve(readyWireMock);
+    } catch (err) {
+      reject(err);
+    }
+  });
+
+  const timeout = new Promise((resolve, reject) =>
+    timeoutHandle = setTimeout(
+      () => reject('Wiremock unavailable after 30s.'),
+      30000));
+  return Promise.race([waitingWireMockPromise, timeout])
+    .then(result => {
+      clearTimeout(timeoutHandle);
+      return result;
+    });
+}
+
+async function waitForWiremockStarted(wireMock, counter) {
+  return fetch(wireMock.baseUri)
+    .then(async (resp) => {
+      if (resp.ok) {
+        return Promise.resolve(wireMock);
+      } else {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        Logger.getInstance().info(`Retry connection to WireMock after wrong response status: ${resp.status}`);
+        if (++counter < 30) { //stop after 30s
+          return await waitForWiremockStarted(wireMock, counter);
+        } else {
+          Promise.reject('Wiremock: Waiting time has expired');
+        }
+      }
+    })
+    .catch(async (err) => {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      Logger.getInstance().info(`Retry connection to WireMock after error: ${err}`);
+      if (++counter < 30) { //stop after 30s
+        return await waitForWiremockStarted(wireMock, counter);
+      } else {
+        Promise.reject('Wiremock: Waiting time has expired');
+      }
+    });
+}
+
+async function addWireMockMappingsFromFile(wireMock, filePath) {
+  const requests = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  for (const mapping of requests.mappings) {
+    await wireMock.mappings.createMapping(mapping);
+  }
+}
+
+exports.runWireMockAsync = runWireMockAsync;
+exports.addWireMockMappingsFromFile = addWireMockMappingsFromFile;
+

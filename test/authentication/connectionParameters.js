e86859b0c9f8e244bd4655cb052ab5bb9a368f8c
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2025-03-27 10:14:32 +0100

    SNOW-1825499 - added experimental flag for new oauth authentication flows

--- test/authentication/connectionParameters.js
+++ test/authentication/connectionParameters.js
@@ -64,6 +64,7 @@ const oauthPATOnWiremock =
       host: 'localhost',
       protocol: 'http',
       authenticator: 'PROGRAMMATIC_ACCESS_TOKEN',
+      enableExperimentalAuthentication: true
     };
 
 const oauthAuthorizationCodeOnWiremock =
@@ -81,7 +82,8 @@ const oauthAuthorizationCodeOnWiremock =
       oauthAuthorizationUrl: 'http://localhost:8099/oauth/authorize',
       oauthRedirectUri: 'http://localhost:8009/snowflake/oauth-redirect',
       oauthScope: 'session:role:ANALYST test-scope',
-      oauthHttpAllowed: true
+      oauthHttpAllowed: true,
+      enableExperimentalAuthentication: true
     };
 
 const oauthClientCredentialsOnWiremock =
@@ -96,7 +98,8 @@ const oauthClientCredentialsOnWiremock =
       authenticator: 'OAUTH_CLIENT_CREDENTIALS',
       oauthClientId: '123',
       oauthClientSecret: 'clientSecret',
-      oauthHttpAllowed: true
+      oauthHttpAllowed: true,
+      enableExperimentalAuthentication: true
     };
 
 const keypairPrivateKey =

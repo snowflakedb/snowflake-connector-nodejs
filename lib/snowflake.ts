f57769f9330ecaa7466563add5d0a15cf3bcb65f
Ruslan Savenok <ruslan.savenok@snowflake.com>
2025-05-20 19:03:44 +0200

    fx

--- lib/snowflake.ts
+++ lib/snowflake.ts
@@ -1,14 +1,14 @@
 const core = require('./core');
 const Util = require('./util');
 import { createClientError, codes as ErrorCodes } from './errors';
-const testFn = require('./__dummyTsFile').default;
+import testFn from './__dummyTsFile';
 
 testFn();
 
 const clientEnvironment = process.versions;
 
 // if we're not using the minimum supported version of node.js, raise an error
-const minimumNodeVersion = '6.0.0';
+const minimumNodeVersion = '6.0.0'; // TODO: this should be node 18 like our docs says?
 if (Util.string.compareVersions(clientEnvironment.node, minimumNodeVersion) < 0) {
   throw createClientError(
     ErrorCodes.ERR_UNSUPPORTED_NODE_JS_VERSION, true, minimumNodeVersion);

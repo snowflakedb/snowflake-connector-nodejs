faa1a7f46e48195e5e89d3087a9177c91d49140e
Ruslan Savenok <ruslan.savenok@snowflake.com>
2025-06-18 19:27:01 +0200

    Fix workload identity config options
    1. feature flag in evn variable instead of ConnectionOptions to be consistent with other drivers
    2. workloadIdentity object replaced with 2 separate keys to work in .toml files

--- lib/connection/types.ts
+++ lib/connection/types.ts
@@ -17,21 +17,19 @@ export interface WIP_ConnectionConfig {
    */
   oauthEnableSingleUseRefreshTokens?: boolean;
 
-  enableExperimentalWorkloadIdentityAuth?: boolean;
-  workloadIdentity?: {
-    /**
-     * Specifies the workload identity provider, available options:
-     * * AWS - Uses `@aws-sdk` to find credentials and encodes signed GetCallerIdentity request as token
-     * * AZURE - Uses `@azure/identity` find credentials and get JWT token
-     * * GCP - Uses `google-auth-library` to find credentials and get JWT token
-     * * OIDC - Reads JWT token from `ConnectionOptions.token`
-     *
-     * When none is passed, the driver will try to auto-detect the provider.
-     */
-    provider?: WorkloadIdentityProviderKey;
-    /**
-     * Customize Azure Entra Id Resource used to obrain auth token
-     */
-    azureEntraIdResource?: string;
-  }
+  /**
+   * When authenticator=WORKLOAD_IDENTITY, specifies the identity provider. Available options:
+   * * AWS - Uses `@aws-sdk` to find credentials and encodes signed GetCallerIdentity request as token
+   * * AZURE - Uses `@azure/identity` find credentials and get JWT token
+   * * GCP - Uses `google-auth-library` to find credentials and get JWT token
+   * * OIDC - Reads JWT token from `ConnectionOptions.token`
+   *
+   * When none is passed, the driver will try to auto-detect the provider.
+   */
+  workloadIdentityProvider?: WorkloadIdentityProviderKey;
+
+  /**
+   * Customize Azure Entra Id Resource used to obrain workload identity auth token
+   */
+  workloadIdentityAzureEntraIdResource?: string;
 }

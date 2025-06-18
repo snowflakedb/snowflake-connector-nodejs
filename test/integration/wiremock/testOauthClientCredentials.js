6a07c8a29b5854e2651b86d53ce38ec3f0f065f2
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2025-06-18 17:11:37 +0200

    SNOW-2113974 Enable single use of refresh token (#1097)
    Co-authored-by: Ruslan Savenok <ruslan.savenok@snowflake.com>

--- test/integration/wiremock/testOauthClientCredentials.js
+++ test/integration/wiremock/testOauthClientCredentials.js
@@ -11,12 +11,11 @@ describe('Oauth Client Credentials authentication', function () {
   before(async () => {
     port = await getFreePort();
     wireMock = await runWireMockAsync(port);
-    connectionOption = { ...connParameters.oauthClientCredentialsOnWiremock,
-      ...{
-        port: port,
-        oauthAuthorizationUrl: `https://127.0.0.1:${port}/oauth/authorize`,
-        oauthTokenRequestUrl: `http://127.0.0.1:${port}/oauth/token-request`,
-      }
+    connectionOption = {
+      ...connParameters.oauthClientCredentialsOnWiremock,
+      port: port,
+      oauthAuthorizationUrl: `https://127.0.0.1:${port}/oauth/authorize`,
+      oauthTokenRequestUrl: `http://127.0.0.1:${port}/oauth/token-request`,
     };
   });
   beforeEach(async () => {
@@ -54,6 +53,20 @@ describe('Oauth Client Credentials authentication', function () {
     authTest.createConnection(connectionOption);
     await authTest.connectAsync();
     authTest.verifyErrorWasThrown('Error while getting access token. Message: "response" body "access_token" property must be a string');
+  });
+
+
+  //no token in response
+  it('Successful flow scenario with single use refresh token - no token', async function () {
+    await addWireMockMappingsFromFile(wireMock, 'wiremock/mappings/oauth/client_credentials/successful_flow_with_single_use_refresh_tokens.json');
+    const extendedConnectionOption = {
+      ...connectionOption,
+      oauthEnableSingleUseRefreshTokens: true
+    };
+    authTest.createConnection(extendedConnectionOption);
+    await authTest.connectAsync();
+    authTest.verifyNoErrorWasThrown();
+    await authTest.verifyConnectionIsUp();
   });
 
 });

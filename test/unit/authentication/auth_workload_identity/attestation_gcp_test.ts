f5064bd277daff611609c88e216a537fce3d1016
John Yun <jonghun.yun@insightsoftware.com>
2025-07-23 13:16:25 -0700

    testing

--- test/unit/authentication/auth_workload_identity/attestation_gcp_test.ts
+++ test/unit/authentication/auth_workload_identity/attestation_gcp_test.ts
@@ -1,45 +1,53 @@
 import sinon from 'sinon';
 import assert from 'assert';
-import { GoogleAuth } from 'google-auth-library';
 import {
   getGcpAttestationToken,
   SNOWFLAKE_AUDIENCE,
 } from '../../../../lib/authentication/auth_workload_identity/attestation_gcp';
 
-describe('Attestation GCP', () => {
-  const sinonSandbox = sinon.createSandbox();
-  let getIdTokenClientStub: sinon.SinonStub;
-  let fetchIdTokenStub: sinon.SinonStub;
+let hasGoogleAuth = true;
+try {
+  require.resolve('google-auth-library');
+} catch (err) {
+  hasGoogleAuth = false;
+}
+if (hasGoogleAuth) {
+  describe('Attestation GCP', () => {
+    const GoogleAuth = require('google-auth-library').GoogleAuth;
+    const sinonSandbox = sinon.createSandbox();
+    let getIdTokenClientStub: sinon.SinonStub;
+    let fetchIdTokenStub: sinon.SinonStub;
 
-  beforeEach(async () => {
-    fetchIdTokenStub = sinon.stub();
-    getIdTokenClientStub = sinon.stub().returns({
-      idTokenProvider: {
-        fetchIdToken: fetchIdTokenStub,
-      },
+    beforeEach(async () => {
+      fetchIdTokenStub = sinon.stub();
+      getIdTokenClientStub = sinon.stub().returns({
+        idTokenProvider: {
+          fetchIdToken: fetchIdTokenStub,
+        },
+      });
+      sinonSandbox.stub(GoogleAuth.prototype, 'getIdTokenClient').callsFake(getIdTokenClientStub);
     });
-    sinonSandbox.stub(GoogleAuth.prototype, 'getIdTokenClient').callsFake(getIdTokenClientStub);
-  });
 
-  afterEach(() => {
-    sinonSandbox.restore();
-  });
+    afterEach(() => {
+      sinonSandbox.restore();
+    });
 
-  it('calls fetchIdToken with the correct audience', async () => {
-    await getGcpAttestationToken();
-    assert.strictEqual(getIdTokenClientStub.firstCall.args[0], SNOWFLAKE_AUDIENCE);
-    assert.strictEqual(fetchIdTokenStub.firstCall.args[0], SNOWFLAKE_AUDIENCE);
-  });
+    it('calls fetchIdToken with the correct audience', async () => {
+      await getGcpAttestationToken();
+      assert.strictEqual(getIdTokenClientStub.firstCall.args[0], SNOWFLAKE_AUDIENCE);
+      assert.strictEqual(fetchIdTokenStub.firstCall.args[0], SNOWFLAKE_AUDIENCE);
+    });
 
-  it('returns null when the token is not found', async () => {
-    fetchIdTokenStub.throws(new Error('Token not found'));
-    const token = await getGcpAttestationToken();
-    assert.strictEqual(token, null);
-  });
+    it('returns null when the token is not found', async () => {
+      fetchIdTokenStub.throws(new Error('Token not found'));
+      const token = await getGcpAttestationToken();
+      assert.strictEqual(token, null);
+    });
 
-  it('returns the token when it is found', async () => {
-    fetchIdTokenStub.resolves('test-token');
-    const token = await getGcpAttestationToken();
-    assert.strictEqual(token, 'test-token');
+    it('returns the token when it is found', async () => {
+      fetchIdTokenStub.resolves('test-token');
+      const token = await getGcpAttestationToken();
+      assert.strictEqual(token, 'test-token');
+    });
   });
-});
+}

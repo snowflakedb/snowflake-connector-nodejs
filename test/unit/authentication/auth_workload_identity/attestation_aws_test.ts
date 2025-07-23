d1fd18d187121628ff726e6fd5422dcf7c434b54
John Yun <jonghun.yun@insightsoftware.com>
2025-07-23 13:53:26 -0700

    fix

--- test/unit/authentication/auth_workload_identity/attestation_aws_test.ts
+++ test/unit/authentication/auth_workload_identity/attestation_aws_test.ts
@@ -3,97 +3,112 @@ import assert from 'assert';
 import rewiremock from 'rewiremock/node';
 import * as OriginalAttestationAws from '../../../../lib/authentication/auth_workload_identity/attestation_aws';
 import { assertAwsAttestationToken, AWS_CREDENTIALS, AWS_REGION } from './test_utils';
+let hasAWSCredential = true;
+try {
+  require.resolve('@aws-sdk/credential-provider-node');
+  require.resolve('@aws-sdk/ec2-metadata-service');
+  require.resolve('@aws-sdk/protocol-http');
+  require.resolve('@aws-sdk/signature-v4');
+  require.resolve('@aws-crypto/sha256-js');
+} catch (err) {
+  hasAWSCredential = false;
+}
 
-describe('Attestation AWS', () => {
-  const sinonSandbox = sinon.createSandbox();
-  const awsSdkMock = {
-    getCredentials: sinonSandbox.stub(),
-    getMetadataRegion: sinonSandbox.stub(),
-  };
-  let AttestationAws: typeof OriginalAttestationAws;
+if (hasAWSCredential) {
+  describe('Attestation AWS', () => {
+    const sinonSandbox = sinon.createSandbox();
+    const awsSdkMock = {
+      getCredentials: sinonSandbox.stub(),
+      getMetadataRegion: sinonSandbox.stub(),
+    };
+    let AttestationAws: typeof OriginalAttestationAws;
 
-  before(async () => {
-    // NOTE:
-    // Sinon can't stub frozen AWS SDK properties, so we need to mock entire require
-    rewiremock('@aws-sdk/credential-provider-node').with({
-      defaultProvider: () => awsSdkMock.getCredentials,
-    });
-    rewiremock('@aws-sdk/ec2-metadata-service').with({
-      MetadataService: class {
-        request = () => awsSdkMock.getMetadataRegion();
-      },
-    });
-    rewiremock.enable();
-    AttestationAws = await import(
-      '../../../../lib/authentication/auth_workload_identity/attestation_aws'
-    );
-  });
-
-  beforeEach(() => {
-    sinonSandbox.restore();
-    awsSdkMock.getCredentials.throws(new Error('No credentials found'));
-    awsSdkMock.getMetadataRegion.throws(new Error('No region found'));
-  });
-
-  after(() => {
-    rewiremock.disable();
-  });
-
-  describe('getAwsCredentials', () => {
-    it('returns null when no credentials are found', async () => {
-      assert.strictEqual(await AttestationAws.getAwsCredentials(), null);
-    });
-
-    it('returns credentials when credentials are found', async () => {
-      awsSdkMock.getCredentials.returns(AWS_CREDENTIALS);
-      assert.strictEqual(await AttestationAws.getAwsCredentials(), AWS_CREDENTIALS);
-    });
-  });
-
-  describe('getAwsRegion', () => {
-    it('returns process.env.AWS_REGION when available', async () => {
-      sinonSandbox.stub(process, 'env').value({ AWS_REGION: 'region-from-env' });
-      assert.strictEqual(await AttestationAws.getAwsRegion(), 'region-from-env');
-    });
-
-    it('returns null when metadata service fails', async () => {
-      assert.strictEqual(await AttestationAws.getAwsRegion(), null);
-    });
-
-    it('returns region when metadata service returns a region', async () => {
-      awsSdkMock.getMetadataRegion.returns(AWS_REGION);
-      assert.strictEqual(await AttestationAws.getAwsRegion(), AWS_REGION);
-    });
-  });
-
-  describe('getStsHostname', () => {
-    it('returns valid name for china region', () => {
-      assert.strictEqual(
-        AttestationAws.getStsHostname('cn-northwest-1'),
-        'sts.cn-northwest-1.amazonaws.com.cn',
+    before(async () => {
+      // NOTE:
+      // Sinon can't stub frozen AWS SDK properties, so we need to mock entire require
+      rewiremock('@aws-sdk/credential-provider-node').with({
+        defaultProvider: () => awsSdkMock.getCredentials,
+      });
+      rewiremock('@aws-sdk/ec2-metadata-service').with({
+        MetadataService: class {
+          request = () => awsSdkMock.getMetadataRegion();
+        },
+      });
+      rewiremock.enable();
+      AttestationAws = await import(
+        '../../../../lib/authentication/auth_workload_identity/attestation_aws'
       );
     });
 
-    it('returns valid name for non-china region', () => {
-      assert.strictEqual(AttestationAws.getStsHostname('us-east-1'), 'sts.us-east-1.amazonaws.com');
+    beforeEach(() => {
+      sinonSandbox.restore();
+      awsSdkMock.getCredentials.throws(new Error('No credentials found'));
+      awsSdkMock.getMetadataRegion.throws(new Error('No region found'));
+    });
+
+    after(() => {
+      rewiremock.disable();
+    });
+
+    describe('getAwsCredentials', () => {
+      it('returns null when no credentials are found', async () => {
+        assert.strictEqual(await AttestationAws.getAwsCredentials(), null);
+      });
+
+      it('returns credentials when credentials are found', async () => {
+        awsSdkMock.getCredentials.returns(AWS_CREDENTIALS);
+        assert.strictEqual(await AttestationAws.getAwsCredentials(), AWS_CREDENTIALS);
+      });
+    });
+
+    describe('getAwsRegion', () => {
+      it('returns process.env.AWS_REGION when available', async () => {
+        sinonSandbox.stub(process, 'env').value({ AWS_REGION: 'region-from-env' });
+        assert.strictEqual(await AttestationAws.getAwsRegion(), 'region-from-env');
+      });
+
+      it('returns null when metadata service fails', async () => {
+        assert.strictEqual(await AttestationAws.getAwsRegion(), null);
+      });
+
+      it('returns region when metadata service returns a region', async () => {
+        awsSdkMock.getMetadataRegion.returns(AWS_REGION);
+        assert.strictEqual(await AttestationAws.getAwsRegion(), AWS_REGION);
+      });
+    });
+
+    describe('getStsHostname', () => {
+      it('returns valid name for china region', () => {
+        assert.strictEqual(
+          AttestationAws.getStsHostname('cn-northwest-1'),
+          'sts.cn-northwest-1.amazonaws.com.cn',
+        );
+      });
+
+      it('returns valid name for non-china region', () => {
+        assert.strictEqual(
+          AttestationAws.getStsHostname('us-east-1'),
+          'sts.us-east-1.amazonaws.com',
+        );
+      });
+    });
+
+    describe('getAwsAttestationToken', () => {
+      it('returns null when no credentials are found', async () => {
+        assert.strictEqual(await AttestationAws.getAwsAttestationToken(), null);
+      });
+
+      it('returns null when no region is found', async () => {
+        awsSdkMock.getCredentials.returns(AWS_CREDENTIALS);
+        assert.strictEqual(await AttestationAws.getAwsAttestationToken(), null);
+      });
+
+      it('returns a valid attestation token', async () => {
+        awsSdkMock.getCredentials.returns(AWS_CREDENTIALS);
+        awsSdkMock.getMetadataRegion.returns(AWS_REGION);
+        const token = await AttestationAws.getAwsAttestationToken();
+        assertAwsAttestationToken(token, AWS_REGION);
+      });
     });
   });
-
-  describe('getAwsAttestationToken', () => {
-    it('returns null when no credentials are found', async () => {
-      assert.strictEqual(await AttestationAws.getAwsAttestationToken(), null);
-    });
-
-    it('returns null when no region is found', async () => {
-      awsSdkMock.getCredentials.returns(AWS_CREDENTIALS);
-      assert.strictEqual(await AttestationAws.getAwsAttestationToken(), null);
-    });
-
-    it('returns a valid attestation token', async () => {
-      awsSdkMock.getCredentials.returns(AWS_CREDENTIALS);
-      awsSdkMock.getMetadataRegion.returns(AWS_REGION);
-      const token = await AttestationAws.getAwsAttestationToken();
-      assertAwsAttestationToken(token, AWS_REGION);
-    });
-  });
-});
+}

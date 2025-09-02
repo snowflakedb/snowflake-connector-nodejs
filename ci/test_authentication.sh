8d61c217fbb6f627393d90876a26a479a6e5ae85
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-09-02 17:03:16 +0200

    first version mfa tests

--- ci/test_authentication.sh
+++ ci/test_authentication.sh
@@ -15,5 +15,5 @@ docker run \
   -v $(cd $THIS_DIR/.. && pwd):/mnt/host \
   -v $WORKSPACE:/mnt/workspace \
   --rm \
-  nexus.int.snowflakecomputing.com:8086/docker/snowdrivers-test-external-browser:13 \
+  nexus.int.snowflakecomputing.com:8086/docker/snowdrivers-test-external-browser-mac:14 \
   "/mnt/host/ci/container/test_authentication.sh"

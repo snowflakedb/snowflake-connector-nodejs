ee75df4c14081872ad1d193cecd5f3e8841a886c
Ruslan Savenok <ruslan.savenok@snowflake.com>
2025-09-30 11:49:50 +0200

    Updated gpg and better logs

--- test/auth-workload-identity-e2e.ts
+++ test/auth-workload-identity-e2e.ts
@@ -10,24 +10,14 @@ const snowflake = require('../lib/snowflake');
 // * Set environment variable PARAMETERS_SECRET
 // * Run ci/test_wif.sh
 describe('Workload Identity Authentication E2E', () => {
-  const account = process.env.SNOWFLAKE_TEST_WIF_ACCOUNT;
-  const host = process.env.SNOWFLAKE_TEST_WIF_HOST;
-  const provider = process.env.SNOWFLAKE_TEST_WIF_PROVIDER;
-  const impersonationPath = process.env.SNOWFLAKE_TEST_WIF_IMPERSONATION_PATH;
-  const expectedUsername = process.env.SNOWFLAKE_TEST_WIF_USERNAME;
-  const expectedUsernameImpersonation = process.env.SNOWFLAKE_TEST_WIF_USERNAME_IMPERSONATION;
-
-  // NOTE: TypeScript needs such if instead of loop for proper type checking
-  if (
-    !account ||
-    !host ||
-    !provider ||
-    !impersonationPath ||
-    !expectedUsername ||
-    !expectedUsernameImpersonation
-  ) {
-    throw new Error(`Test can run only on cloud VM with env variables set.`);
-  }
+  const account = getValueFromEnv('SNOWFLAKE_TEST_WIF_ACCOUNT');
+  const host = getValueFromEnv('SNOWFLAKE_TEST_WIF_HOST');
+  const provider = getValueFromEnv('SNOWFLAKE_TEST_WIF_PROVIDER');
+  const impersonationPath = getValueFromEnv('SNOWFLAKE_TEST_WIF_IMPERSONATION_PATH');
+  const expectedUsername = getValueFromEnv('SNOWFLAKE_TEST_WIF_USERNAME');
+  const expectedUsernameImpersonation = getValueFromEnv(
+    'SNOWFLAKE_TEST_WIF_USERNAME_IMPERSONATION',
+  );
 
   const connectionOptions: WIP_ConnectionOptions = {
     authenticator: 'WORKLOAD_IDENTITY',
@@ -40,6 +30,8 @@ describe('Workload Identity Authentication E2E', () => {
     await connectAndVerify(connectionOptions, expectedUsername);
   });
 
+  // NOTE:
+  // GCP token works as OIDC token, so we test it on GCP VM but with different username
   if (provider === 'GCP') {
     it('connects using OIDC', async () => {
       const token = execSync(
@@ -89,4 +81,12 @@ async function connectAndVerify(
     });
   });
   assert.deepEqual(rows, [{ 'CURRENT_USER()': expectedUsername }]);
+}
+
+function getValueFromEnv(key: string) {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Test requires ${key} variable to be set`);
+  }
+  return value;
 }

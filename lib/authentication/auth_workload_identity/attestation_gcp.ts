1987e070e212e6d60e3a118e3b742105aafc8c50
Ruslan Savenok <ruslan.savenok@snowflake.com>
2025-10-03 17:29:37 +0200

    test

--- lib/authentication/auth_workload_identity/attestation_gcp.ts
+++ lib/authentication/auth_workload_identity/attestation_gcp.ts
@@ -1,4 +1,4 @@
-import { GoogleAuth } from 'google-auth-library';
+import { GoogleAuth, Impersonated } from 'google-auth-library';
 import Logger from '../../logger';
 
 export const SNOWFLAKE_AUDIENCE = 'snowflakecomputing.com';
@@ -7,46 +7,10 @@ export async function getGcpAttestationToken(impersonationPath: string[] = []) {
   let auth = new GoogleAuth();
 
   for (const serviceAccount of impersonationPath) {
-    Logger().debug(`Getting GCP auth token from service account: ${serviceAccount}`);
-    const authClient = await auth.getClient();
-    const accessTokenResponse = await authClient.getAccessToken();
-    if (!accessTokenResponse.token) {
-      throw new Error(`Failed to get access token from service account: ${serviceAccount}`);
-    }
-
-    const credentialsResponse = await fetch(
-      `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${serviceAccount}:generateAccessToken`,
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${accessTokenResponse.token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          scope: ['https://www.googleapis.com/auth/cloud-platform'],
-          lifetime: '3600s',
-        }),
-      },
-    );
-
-    if (!credentialsResponse.ok) {
-      const errorText = await credentialsResponse.text();
-      throw new Error(`HTTP ${credentialsResponse.status}: ${errorText}`);
-    }
-
-    const responseData = (await credentialsResponse.json()) as { accessToken?: string };
-
-    if (responseData.accessToken) {
-      throw new Error(
-        `Failed to get credentials from impersonation service account ${serviceAccount}`,
-      );
-    }
-
-    auth = new GoogleAuth({
-      credentials: {
-        type: 'service_account',
-        refresh_token: responseData.accessToken,
-      },
+    // @ts-ignore
+    auth = new Impersonated({
+      sourceClient: await auth.getClient(),
+      targetPrincipal: serviceAccount,
     });
   }
 
@@ -55,3 +19,79 @@ export async function getGcpAttestationToken(impersonationPath: string[] = []) {
   const idToken = await client.idTokenProvider.fetchIdToken(SNOWFLAKE_AUDIENCE);
   return idToken;
 }
+
+// import { GoogleAuth, Impersonated } from 'google-auth-library';
+
+// export const SNOWFLAKE_AUDIENCE = 'snowflakecomputing.com';
+
+// export async function getGcpAttestationToken() {
+//   const auth = new GoogleAuth();
+
+//   Logger().debug('Getting GCP auth token');
+
+//   // Obtain the default credentials
+//   const { credential } = await auth.getApplicationDefault();
+
+//   // Define the impersonated service account and scopes
+//   const impersonatedServiceAccount = 'name@project.service.gserviceaccount.com'; // Replace with your service account
+//   const targetScopes = ['https://www.googleapis.com/auth/cloud-platform'];
+
+//   // Create the impersonated credentials
+//   const impersonatedCredentials = new Impersonated({
+//     sourceClient: credential,
+//     targetPrincipal: impersonatedServiceAccount,
+//     targetScopes,
+//     lifetime: 300, // Token lifetime in seconds
+//   });
+
+//   // Fetch the ID token for the target audience
+//   const idToken = await impersonatedCredentials.fetchIdToken(SNOWFLAKE_AUDIENCE, {
+//     includeEmail: true,
+//   });
+
+//   return idToken;
+// }
+
+// for (const serviceAccount of impersonationPath) {
+//   Logger().debug(`Getting GCP auth token from service account: ${serviceAccount}`);
+//   const authClient = await auth.getClient();
+//   const accessTokenResponse = await authClient.getAccessToken();
+//   if (!accessTokenResponse.token) {
+//     throw new Error(`Failed to get access token from service account: ${serviceAccount}`);
+//   }
+
+//   const credentialsResponse = await fetch(
+//     `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${serviceAccount}:generateAccessToken`,
+//     {
+//       method: 'POST',
+//       headers: {
+//         Authorization: `Bearer ${accessTokenResponse.token}`,
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         scope: ['https://www.googleapis.com/auth/cloud-platform'],
+//         lifetime: '3600s',
+//       }),
+//     },
+//   );
+
+//   if (!credentialsResponse.ok) {
+//     const errorText = await credentialsResponse.text();
+//     throw new Error(`HTTP ${credentialsResponse.status}: ${errorText}`);
+//   }
+
+//   const responseData = (await credentialsResponse.json()) as { accessToken?: string };
+
+//   if (responseData.accessToken) {
+//     throw new Error(
+//       `Failed to get credentials from impersonation service account ${serviceAccount}`,
+//     );
+//   }
+
+//   auth = new GoogleAuth({
+//     credentials: {
+//       type: 'service_account',
+//       refresh_token: responseData.accessToken,
+//     },
+//   });
+// }

fb029848c63b020a49026ad4587085ab8696d877
Rafal Ratajczyk <rafal.ratajczyk@niceproject.eu>
2025-10-15 15:07:24 +0200

    - fix issue with unresolved azure package in attestation_azure

--- lib/authentication/auth_workload_identity/attestation_azure.ts
+++ lib/authentication/auth_workload_identity/attestation_azure.ts
@@ -1,5 +1,3 @@
-// @ts-ignore
-import { DefaultAzureCredential } from '@azure/identity';
 import Logger from '../../logger';
 
 export const DEFAULT_AZURE_ENTRA_ID_RESOURCE = 'api://fd3f753b-eed3-462c-b6a7-a4b5bb650aad';
@@ -10,6 +8,10 @@ export async function getAzureAttestationToken(
     entraIdResource?: string;
   } = {},
 ) {
+  // @ts-ignore
+  const DefaultAzureCredential = await import('@azure/identity').then(
+    (e) => e.DefaultAzureCredential,
+  );
   const credential = new DefaultAzureCredential({
     managedIdentityClientId: options.managedIdentityClientId,
   });

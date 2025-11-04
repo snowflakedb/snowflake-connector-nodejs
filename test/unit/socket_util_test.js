d094a4a7a488c321d02567d7884b8e39cc7f0399
sfc-gh-dszmolka <david.szmolka@snowflake.com>
2025-11-04 18:03:07 +0100

    added a test for ProxyAgent creation

---
+++ test/unit/socket_util_test.js
@@ -0,0 +1,27 @@
+const ProxyAgent = require('./../../lib/agent/https_proxy_agent');
+
+const assert = require('assert');
+
+describe('SocketUtil Test - simulating ProxyAgent creation', () => {
+  const HttpProxyUtilFromEnvProxy = { host: 'localhost', port: 8080, protocol: 'http:', noProxy: undefined, useForOCSP: true };
+  const HttpsProxyUtilFromEnvProxy = { host: 'localhost', port: 8080, protocol: 'https:', noProxy: undefined, useForOCSP: true };
+
+  [
+    {
+      name: 'create agent with HTTP_PROXY',
+      agent:  new ProxyAgent(HttpProxyUtilFromEnvProxy),
+      shouldMatch: HttpProxyUtilFromEnvProxy,
+    },
+      {
+        name: 'create agent with HTTPS_PROXY',
+        agent:  new ProxyAgent(HttpsProxyUtilFromEnvProxy),
+        shouldMatch: HttpsProxyUtilFromEnvProxy,
+      }
+  ].forEach(({ name, agent, shouldMatch }) => {
+    let agentWithoutClosingSlash;
+    agentWithoutClosingSlash = agent.proxy.href.slice(0, -1);
+    it(`${name}`, () => {
+      assert.deepEqual(agentWithoutClosingSlash, shouldMatch, `proxy string from within ProxyAgent was ${agentWithoutClosingSlash}, expected ${shouldMatch}`);
+    });
+  });
+});

df9356d6ed063c5d83d1972d570cea19262c49b2
Ruslan Savenok <ruslan.savenok@snowflake.com>
2025-11-24 17:52:15 +0100

    test

--- .mocharc.js
+++ .mocharc.js
@@ -4,8 +4,13 @@ module.exports = {
   fullTrace: true,
   recursive: true,
   extension: ['js', 'ts'],
-  parallel: true,
-  jobs: 4,
+  // TODO: Enable parallel test execution
+  // - 4 parallel jobs provide optimal performance (3x faster execution)
+  // - More than 4 jobs don't improve performance further
+  // - Parallel execution currently causes some tests to become flaky
+  // parallel: true,
+  // jobs: 4,
+
   // TODO:
   // Consider removing retries once connectivity issues resolved in SNOW-2679711
   retries: 1,

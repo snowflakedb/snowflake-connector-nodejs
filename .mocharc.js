8df472175a14a7419c77b0ca0477e8388fa167b1
Ruslan Savenok <ruslan.savenok@snowflake.com>
2025-11-24 16:20:35 +0100

    NO-SNOW - Improve tests pipeline

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,19 @@
+const snowflake = require('./lib/snowflake');
+
 module.exports = {
   require: ['ts-node/register'],
   timeout: 180000,
   fullTrace: true,
   recursive: true,
   extension: ['js', 'ts'],
+  parallel: true,
+  jobs: 4,
+  retries: 1,
+  rootHooks: {
+    beforeEach: () => {
+      // NOTE:
+      // Some tests change it, so beforeAll is not enough to force trace for every test
+      snowflake.configure({ logLevel: 'trace' });
+    },
+  },
 };

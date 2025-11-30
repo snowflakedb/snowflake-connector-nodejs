2fe2674385f01a9002987d5e237562b15cfc360e
Ruslan Savenok <ruslan.savenok@snowflake.com>
2025-11-30 08:57:46 +0100

    fx

--- test/unit/minicore_test.ts
+++ test/unit/minicore_test.ts
@@ -12,7 +12,7 @@ describe('Minicore', () => {
     assert.strictEqual(version, '0.0.1');
   });
 
-  it('returns failed-to-load when binary fails to load', async () => {
+  it('returns failed-to-load when binary fails to load', () => {
     delete require.cache[require.resolve('../../lib/minicore')];
     sinon.stub(process, 'platform').value('dummy-test-platform');
     const minicoreModule = require('../../lib/minicore') as typeof import('../../lib/minicore');
